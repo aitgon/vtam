68975e76c5c9c812ba24c8011f464b045086b43d
Dechatre Thomas <thomas.dechatre@etu.univ-amu.fr>
2018-03-05 16:24:47 +0100

    UPGRADE

--- wopmetabarcoding/model/File.py
+++ wopmetabarcoding/model/File.py
@@ -1,6 +1,6 @@
 from wopmars.framework.database.Base import Base
 
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Boolean
 from sqlalchemy.orm import validates
 
 
@@ -10,3 +10,4 @@ class File(Base):
 	file_id = Column(Integer, primary_key=True, autoincrement=True)
 	file_name = Column(String(150), nullable=False)
 	run_name = Column(String(20), nullable=False)
+	dereplicate_status = Column(String, nullable=False)
