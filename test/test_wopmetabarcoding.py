291ba7e401a257d75060602b27628b0ce6fbc31c
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2018-05-07 14:22:39 +0200

    TST Fixed tests

--- test/test_wopmetabarcoding.py
+++ test/test_wopmetabarcoding.py
@@ -11,7 +11,7 @@ from jinja2 import Template
 from wopmars.framework.parsing.Parser import ToolWrapper
 
 from wopmetabarcoding.utils.PathFinder import PathFinder
-from wopmetabarcoding.wrapper.FilterUtilities import chimera, lfn1_per_replicate, Variant2Sample2Replicate2Count
+from wopmetabarcoding.wrapper.FilterUtilities import Variant2Sample2Replicate2Count
 from wopmetabarcoding.utils.constants import wopmetabarcoding_filter_test_data
 
 
@@ -26,60 +26,60 @@ class TestWopMetabarcoding(TestCase):
         with open(self.__wopfile_template_path, 'r') as fin:
             self.__wopfile_template_str = fin.read()
 
-    # def test_01_sample_information(self):
-    #     test_outdir = os.path.join(self.__testdir_path, "output", "sample_information")
-    #     PathFinder.mkdir(test_outdir)
-    #     sample_info_csv = os.path.join(PathFinder.get_module_test_path(), "input", "sample_information", "1_sample_info.csv")
-    #     db_path = os.path.join(test_outdir, "db.sqlite")
-    #     db_url = "sqlite:///" + db_path
-    #     #
-    #     # Create wopfile
-    #     template = Template(self.__wopfile_template_str)
-    #     wopfile_str = template.render(SAMPLE_INFORMATION_CSV=sample_info_csv)
-    #     wopfile_path = os.path.join(test_outdir, "Wopfile.yml")
-    #     with open(wopfile_path, 'w') as fout:
-    #         self.__wopfile_template_str = fout.write(wopfile_str)
-    #     #
-    #     cmd_line = ["wopmars", "-w", wopfile_path, "-D", db_url, "-d", PathFinder.get_module_path(), "-F", "-t", "SampleInformation"]
-    #     p = subprocess.Popen(cmd_line)
-    #     p.wait()
-    #     #
-    #     output = (1, 'ZFZR', 'AGATATTGGAACWTTATATTTTATTTTTGG', 'WACTAATCAATTWCCAAATCCTCC', 'cgatcgtcatcacg', 'ctcgatgatcacg', 1, 'prerun', '14Mon01', 'repl1')
-    #     con = sqlite3.connect(self.__db_path)
-    #     cur = con.cursor()
-    #     cur.execute("SELECT * from SampleInformation;")
-    #     self.assertTrue(list(cur.fetchone()) == list(output))
-    #     #
-    #     shutil.rmtree(test_outdir)
-    #
-    # def test_02_sort_reads(self):
-    #     test_outdir = os.path.join(self.__testdir_path, "output", "sort_reads")
-    #     PathFinder.mkdir(test_outdir)
-    #     sample_info_csv = os.path.join(PathFinder.get_module_test_path(), "input", "sort_reads", "1_sample_info.csv")
-    #     db_path = os.path.join(test_outdir, "db.sqlite")
-    #     db_url = "sqlite:///" + db_path
-    #     #
-    #     # Create wopfile
-    #     template = Template(self.__wopfile_template_str)
-    #     wopfile_str = template.render(SAMPLE_INFORMATION_CSV=sample_info_csv)
-    #     wopfile_path = os.path.join(test_outdir, "Wopfile.yml")
-    #     with open(wopfile_path, 'w') as fout:
-    #         self.__wopfile_template_str = fout.write(wopfile_str)
-    #     #
-    #     cmd_line = ["wopmars", "-w", wopfile_path, "-D", db_url, "-d", self.__testdir_path, "-F", "-t", "SortReads"]
-    #     p = subprocess.Popen(cmd_line)
-    #     p.wait()
-    #     #
-    #     marker_id = 1
-    #     sequence = 'AGCCTGAGCTGGAATAGTAGGTACTTCCCTTAGTATACTTATTCGAGCCGAATTAGGACACCCAGGCTCTCTAATTGGAGACGACCAAATTTATAATGTAATTGTTACTGCTCATGCTTTTGTAATAATTTTTTTTATAGTTATGCCAATTATAATT'
-    #     con = sqlite3.connect(db_path)
-    #     cur = con.cursor()
-    #     sql = "SELECT marker_id from Variant where sequence='%s';"%(sequence)
-    #     cur.execute(sql)
-    #     self.assertTrue(list(cur.fetchone()) == [1])
-    #     #
-    #     shutil.rmtree(test_outdir)
-    #
+    def test_01_sample_information(self):
+        test_outdir = os.path.join(self.__testdir_path, "output", "sample_information")
+        PathFinder.mkdir(test_outdir)
+        sample_info_csv = os.path.join(PathFinder.get_module_test_path(), "input", "sample_information", "1_sample_info.csv")
+        db_path = os.path.join(test_outdir, "db.sqlite")
+        db_url = "sqlite:///" + db_path
+        #
+        # Create wopfile
+        template = Template(self.__wopfile_template_str)
+        wopfile_str = template.render(SAMPLE_INFORMATION_CSV=sample_info_csv)
+        wopfile_path = os.path.join(test_outdir, "Wopfile.yml")
+        with open(wopfile_path, 'w') as fout:
+            self.__wopfile_template_str = fout.write(wopfile_str)
+        #
+        cmd_line = ["wopmars", "-w", wopfile_path, "-D", db_url, "-d", PathFinder.get_module_path(), "-F", "-t", "SampleInformation"]
+        p = subprocess.Popen(cmd_line)
+        p.wait()
+        #
+        output = (1, 'ZFZR', 'AGATATTGGAACWTTATATTTTATTTTTGG', 'WACTAATCAATTWCCAAATCCTCC', 'cgatcgtcatcacg', 'ctcgatgatcacg', 1, 'prerun', '14Mon01', 'repl1')
+        con = sqlite3.connect(self.__db_path)
+        cur = con.cursor()
+        cur.execute("SELECT * from SampleInformation;")
+        self.assertTrue(list(cur.fetchone()) == list(output))
+        #
+        shutil.rmtree(test_outdir)
+
+    def test_02_sort_reads(self):
+        test_outdir = os.path.join(self.__testdir_path, "output", "sort_reads")
+        PathFinder.mkdir(test_outdir)
+        sample_info_csv = os.path.join(PathFinder.get_module_test_path(), "input", "sort_reads", "1_sample_info.csv")
+        db_path = os.path.join(test_outdir, "db.sqlite")
+        db_url = "sqlite:///" + db_path
+        #
+        # Create wopfile
+        template = Template(self.__wopfile_template_str)
+        wopfile_str = template.render(SAMPLE_INFORMATION_CSV=sample_info_csv)
+        wopfile_path = os.path.join(test_outdir, "Wopfile.yml")
+        with open(wopfile_path, 'w') as fout:
+            self.__wopfile_template_str = fout.write(wopfile_str)
+        #
+        cmd_line = ["wopmars", "-w", wopfile_path, "-D", db_url, "-d", self.__testdir_path, "-F", "-t", "SortReads"]
+        p = subprocess.Popen(cmd_line)
+        p.wait()
+        #
+        marker_id = 1
+        sequence = 'AGCCTGAGCTGGAATAGTAGGTACTTCCCTTAGTATACTTATTCGAGCCGAATTAGGACACCCAGGCTCTCTAATTGGAGACGACCAAATTTATAATGTAATTGTTACTGCTCATGCTTTTGTAATAATTTTTTTTATAGTTATGCCAATTATAATT'
+        con = sqlite3.connect(db_path)
+        cur = con.cursor()
+        sql = "SELECT marker_id from Variant where sequence='%s';"%(sequence)
+        cur.execute(sql)
+        self.assertTrue(list(cur.fetchone()) == [1])
+        #
+        shutil.rmtree(test_outdir)
+
     # def test_lfn1_per_replicate(self):
     #     #
     #     # Input
@@ -90,16 +90,16 @@ class TestWopMetabarcoding(TestCase):
     #     failed_indices = [309, 353, 55, 237, 377, 263, 354, 408, 145, 178, 240, 287, 319, 339]
     #     self.assertTrue(lfn1_per_replicate(variant2sample2replicate2count_df, lfn_per_replicate_threshold) == failed_indices)
 
-    def test_class_variant2sample2replicate2count(self):
-        #
-        # Input
-        db_sqlite = os.path.join(wopmetabarcoding_filter_test_data, "db.sqlite")
-        ZFZR_sample_count_tsv = os.path.join(wopmetabarcoding_filter_test_data, "ZFZR_sample_count.tsv")
-        variant2sample2replicate2count_df = pandas.read_csv(ZFZR_sample_count_tsv, sep='\t')
-        variant2sample2replicate2count = Variant2Sample2Replicate2Count(variant2sample2replicate2count_df)
-        lfn_per_replicate_threshold = 0.025
-        failed_indices = [309, 353, 55, 237, 377, 263, 354, 408, 145, 178, 240, 287, 319, 339]
-        self.assertTrue(variant2sample2replicate2count.lfn1_per_replicate(lfn_per_replicate_threshold) == failed_indices)
+    # def test_class_variant2sample2replicate2count(self):
+    #     #
+    #     # Input
+    #     db_sqlite = os.path.join(wopmetabarcoding_filter_test_data, "db.sqlite")
+    #     ZFZR_sample_count_tsv = os.path.join(wopmetabarcoding_filter_test_data, "ZFZR_sample_count.tsv")
+    #     variant2sample2replicate2count_df = pandas.read_csv(ZFZR_sample_count_tsv, sep='\t')
+    #     variant2sample2replicate2count = Variant2Sample2Replicate2Count(variant2sample2replicate2count_df)
+    #     lfn_per_replicate_threshold = 0.025
+    #     failed_indices = [309, 353, 55, 237, 377, 263, 354, 408, 145, 178, 240, 287, 319, 339]
+    #     self.assertTrue(variant2sample2replicate2count.lfn1_per_replicate(lfn_per_replicate_threshold) == failed_indices)
 
 
     # def test_03_chimera(self):
