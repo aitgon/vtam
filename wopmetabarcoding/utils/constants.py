471946863c4b3ed233e2a2edcfb83c961ef90576
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2018-05-14 11:03:30 +0200

    TST Fixed tests

--- wopmetabarcoding/utils/constants.py
+++ wopmetabarcoding/utils/constants.py
@@ -3,6 +3,5 @@ import tempfile
 import os
 
 tempdir = tempfile.mkdtemp()
-print(tempdir)
 
 wopmetabarcoding_filter_test_data = os.path.join(os.path.dirname(__file__), "../../../wopmetabarcoding_filter_test_data")
