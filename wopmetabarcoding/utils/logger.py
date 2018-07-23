c23502fca42ca310a5bbabd5dbf53450bf7b0d97
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2018-07-23 21:42:55 +0200

    BUG

--- wopmetabarcoding/utils/logger.py
+++ wopmetabarcoding/utils/logger.py
@@ -13,7 +13,7 @@ loggerHandlerStdout = logging.StreamHandler(sys.stdout)
 
 WOPMETABARCODING_LOG = "wopmetabarcoding.log"
 if "WOPMETABARCODING_LOG" in os.environ:
-    WOPMETABARCODING_LOG = int(os.environ["WOPMETABARCODING_LOG"])
+    WOPMETABARCODING_LOG = os.environ["WOPMETABARCODING_LOG"]
 loggerHandlerFile = logging.FileHandler(WOPMETABARCODING_LOG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
