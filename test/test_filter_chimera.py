412a97ec704f6c7770d678378a385b91ae54ca76
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-03-15 13:48:41 +0100

    ENH Added test chimera

--- test/test_filter_chimera.py
+++ test/test_filter_chimera.py
@@ -12,110 +12,123 @@ import os
 from wopmetabarcoding.utils.constants import tempdir
 
 
-
-
-
-
-
-
 class TestChimera(TestCase):
 
     def setUp(self):
         # Input from min_replicate_number
+        # Variants 1 and 2 are ok but 3-5 are chimeras
         self.variant_df = pandas.DataFrame({
-            'id': list(range(1,6)),
+            'id': list(range(1,7)),
             'sequence': [
-                'TGTTCTTTATTTATTATTTGCTGGTTTTGCTGGTGTTTTAGCTGTAACTTTATCATTATTAATTAGATTACAATTAGTTGCTACTGGGTATGGATGATTAGCTTTGAATTATCAATTTTATAACACTATTGTAACTGCTCATGGATTATTAATAGTATTTTTTCTCCTTATGCCTGCTTTAATAGGTGGTTTTGGTAATTGAATAGTTCCTGTTCTAATTGGTTCTATTGATATGGCTTACCCTAGATTAAATAATATTAGTTTTTGATTATTGCCCCCTAGTTTATTATAATTAGTTGG',
+                'TGTTCTTTATTTATTATTTGCTGGTTTTGCTGGTGTTTTAGCTGTAACTTTATCATTATTAATTAGATTACAATTAGTTGCTACTGGGTATGGATGATTAGCTTTGAATTATCAATTTTATAACACTATTGTAACTGCTCATGGATTATTAATAGTATTTTTTCTCCTTATGCCTGCTTTAATAGGTGGTTTTGGTAATTGAATAGTTCCTGTTCTAATTGGTTCTATTGATATGGCTTACCCTAGATTAAATAATATTAGTTTTTGATTATTGCCCCCTAGTTTATTATTATTAGTTGG',
+                'ACTAACCAGGTGATTTGAAGTAAATTAGTTGAGGATTTAGCCGCGCTATCCGGTAATCTCCAAATTAAAACATACCGTTCCATGAGGGCTAGAATTACTTACCGGCCTTCACCATGCCTGCGCTATACGCGCCCACTCTCCCGTTTATCCGTCCAAGCGGATGCAATGCGATCCTCCGCTAAGATATTCTTACGTGTAACGTAGCTATGTATTTTACAGAGCTGGCGTACGCGTTGAACACTTCACAGATGATAGGGATTCGGGTAAAGAGCGTGTTATTGGGGACTTACACAGGCGTAG',
+                'CCTGGGTGAGCTCGAGACTCGGGGTGACAGCTCTTCATACATAGAGCGGGGGCGTCGAACGGTCGTGAAAGTCATAGTACCCCGGGTACCAACTTACTGAGGATATTGCTTGAAGCTGTACCGTTTTAGGGGGGGAACGCTGAAGATCTCTTCTTCTCATGACTGAACTCGCGAGGGTCGTGATGTCGGTTCCTTCAAAGGTTAAAGAACAAAGGCTTACTGTGCGCAGAGGAACGCCCATTTAGCGGCTGGCGTCTTGAATCCTCGGTCCCCCTTGTCTTTCCAGATTAATCCATTTCC',
                 'AACTATGTACACAAATTTTAGTATATTGGCAGGGATAGTAGGAACTTTACTATCGTTAGTTATCAGAATGGAATTATCAACAGGAAACATGTTAGATGGAGACGGTCAACAATATAACGTAATCGTAACCGCACATGGATTAATAATGATATTCTTCGTGGTTATGCCGGCAATGTTAGGAGGATTTGCAAACTGGTTCATACCAATAATGGTAGGATCACCAGATGTAGCTTTTCCAAGATTAAACAACATTAGCTTATGGTTAATATTATTGCCCCCTAGTTTATTATTATTAGTTGG',
+                'TGTTCTTTATTTATTATTTGCTGGTTTTGCTGGTGTTTTAGCTGTAACTTTATCATTATTAATTAGATTACAATTAGTTGCTACTGGGTATGGATGATTAGCTTTGAATTATCAATTTTATAACACTATTGTAACTGCTCATGGATTATTAATAGTATTTTTTCTCCTTATGCCTGCTTTAATAGGTGGTTTTGGTAATTGAATAGTTCCTGTTCTAATTGGTTCTATTGATATGGCTTACCCTAGATTAAATAATATTAGTTTTTGATTATTGCCCCCTAGTTTATTATAATTAGTTGG',
                 'TGTTCTTTATTTATTATTTGCTGGTTTTGCTGGTGTTTTAGCTGTAACTTTATCATTATTAATTAGATTACAATTAGTTGCTACTGGGTATGGATGATTAGCTTTGAATTATCAATTTTATAACACTATTGTAACTGCTCATGGATTATTATTCTTCGTGGTTATGCCGGCAATGTTAGGAGGATTTGCAAACTGGTTCATACCAATAATGGTAGGATCACCAGATGTAGCTTTTCCAAGATTAAACAACATTAGCTTATGGTTAATATTATTGCCCCCTAGTTTATTATTATTAGTTGG',
-                'TGTTCTTTATTTATTATTTGCTGGTTTTGCTGGTGTTTTAGCTGTAACTTTATCATTATTAATTAGATTACAATTATTTGCTACTGGGTATGGATGATTAGCTTTGAATTATCAATTTTATAACACTATTGTAACTGCTCATGGATTATTATTCTTCGTGGTTATGCCGGCAATGTTAGGAGGATTTGCAAACTGGTTCATACCAATAATGGTAGGATCACCAGATGTAGCTTTTCCAAGATTAAACAACATTAGCTTATGGTTAATATTATTGCCCCCTAGTTTATTATTATTAGTTGG',
-                'TGTTCTTTATTTATTATTTGCTGGTTTTGCTGGTGTTTTAGCTGTAACTTTATCATTATTAATTAGATTACAATTAGTTGCTACTGGGTATGGATGATTAGCTTTGAATTATCAATTTTATAACACTATTGTAACTGCTCATGGATTATTAATAGTATTTTTTCTCCTTATGCCTGCTTTAATAGGTGGTTTTGGTAATTGAATAGTTCCTGTTCTAATTGGTTCTATTGATATGGCTTACCCTAGATTAAATAATATTAGTTTTTGATTATTGCCCCCTAGTTTATTATTATTAGTTGG'
+
                          ],
         })
         #
         self.variant_read_count_df = pandas.DataFrame({
-            'run_id': [1]*10,
-            'marker_id': [1]*10,
-            'variant_id': [1]*2 + [2]*2 + [3]*2 + [4]*2 + [5]*2,
-            'biosample_id': [ 1,2,1,2,1,2,1,2,1, 2],
-            'replicate_id': [1, 2]*5,
+            'run_id': [1] * 12,
+            'marker_id': [1]*12,
+            'variant_id': [6]*2 + [1]*2 + [2]*2 + [3]*2 + [4]*2 + [5]*2,
+            'biosample_id': [1] * 12,
+            'replicate_id': [1, 2] * 6,
             'read_count':[
-                350, 300, 300, 400, 50, 0, 260, 240,  0, 50,
+                25, 25, 350, 360, 335, 325, 350, 350, 325, 325, 35, 25
                   ],
         })
 
-        self.tempdir = os.path.join(tempdir, "FilterUtilities", "FilterUtilities",
-                                    self.__class__.__name__)
+        self.tempdir = os.path.join(tempdir, "FilterUtilities", self.__class__.__name__)
         PathFinder.mkdir_p(self.tempdir)
 
 
-
-        #
-
     def test_02_f11_chimera(self):
+        this_filter_name = 'FilterChimera'
+        filter_output_df = self.variant_read_count_df.copy()
+        filter_output_df['filter_id'] = 12
+        filter_output_df['filter_delete'] = 0
         #
-        # - Test 1: Given variants and read_count for grouped variant for each biosample_id, prepare fasta repl.fas
+        df_grouped_variant_read_count = self.variant_read_count_df.groupby(by=['run_id', 'marker_id', 'variant_id', 'biosample_id']).sum().reset_index()
+        df_grouped_variant_read_count = df_grouped_variant_read_count[['run_id', 'marker_id', 'variant_id', 'biosample_id', 'read_count']] # keep columns
         #
-        this_filter_name="chimera_filter"
-        variant_id_list_of_lists = []
-        sample_list = self.variant_read_count_df.biosample_id.unique().tolist()
+        df_grouped_run_marker_biosample = self.variant_read_count_df.groupby(by=['run_id', 'marker_id', 'biosample_id']).sum().reset_index()
+        df_grouped_run_marker_biosample = df_grouped_run_marker_biosample[['run_id', 'marker_id', 'biosample_id']] # keep columns
+        ###################################################################
+        #
+        # 1. Make a fasta file with all variants for each run, marker, biosample
+        #
+        ###################################################################
+        for row_run_marker_biosample in df_grouped_run_marker_biosample.iterrows():
+            run_id = row_run_marker_biosample[1].run_id
+            marker_id = row_run_marker_biosample[1].marker_id
+            biosample_id = row_run_marker_biosample[1].biosample_id
+            #
+            df_grouped_biosample_id = df_grouped_variant_read_count.loc[df_grouped_variant_read_count.biosample_id==biosample_id, ['variant_id', 'read_count']]
 
-        for row in self.variant_read_count_df[['biosample_id']].drop_duplicates(['biosample_id']).itertuples():
-            biosample_id = row.biosample_id
-            subset_name = "{}".format(biosample_id)
-            df_subset = self.variant_read_count_df.loc[
-                                                     (self.variant_read_count_df.biosample_id == biosample_id)]
-            variant_id_list_of_lists.append(df_subset.variant_id.unique().tolist())
+            ###################################################################
+            #
+            # 2. Sort variants by abundance and write to fasta
+            #
+            ###################################################################
+            df_grouped_biosample_id.sort_values(by='read_count', ascending=False, inplace=True)
+            #
+            chimera1_fasta = os.path.join(self.tempdir, this_filter_name, 'chimera1_biosample_id_{}.fasta'.format(biosample_id))
+            #
+            # Prepare 1 fasta file
+            PathFinder.mkdir_p(os.path.join(self.tempdir, this_filter_name))
+            with open(chimera1_fasta, 'w') as fasta_fout:
+                for variant_id in df_grouped_biosample_id.variant_id.unique():
+                    variant_sequence = self.variant_df.loc[self.variant_df.id == variant_id,'sequence'].values[0]
+                    read_count = df_grouped_biosample_id.loc[df_grouped_variant_read_count.variant_id == variant_id, 'read_count'].values[0]
+                    fasta_fout.write('>{};size={}\n{}\n'.format(variant_id, read_count, variant_sequence))
 
-        for variant_id_list in variant_id_list_of_lists:
+            ###################################################################
+            #
+            # 3. Run uchime_denovo
+            #
+            ###################################################################
+            chimear2_borderline_fasta = os.path.join(self.tempdir, this_filter_name,'chimear2_biosample_id_{}_borderline.fasta'.format(biosample_id))
+            chimear2_nonchimeras_fasta = os.path.join(self.tempdir, this_filter_name,'chimear2_biosample_id_{}_nonchimeras.fasta'.format(biosample_id))
+            chimear2_chimeras_fasta = os.path.join(self.tempdir, this_filter_name,'chimear2_biosample_id_{}_chimeras.fasta'.format(biosample_id))
+            #
+            vsearch_chimera_args = {
+                "uchime_denovo": chimera1_fasta,
+                "borderline": chimear2_borderline_fasta,
+                "nonchimeras": chimear2_nonchimeras_fasta,
+                "chimeras": chimear2_chimeras_fasta
+            }
+            vsearch_chimera = Vsearch3(**vsearch_chimera_args)
+            vsearch_chimera.run()
 
-                ###################################################################
-                # 1. Make a fasta file with all variants of the sample or replicate
-                ###################################################################
-                PathFinder.mkdir_p(os.path.join(self.tempdir, this_filter_name))
-                subset_fasta = os.path.join(self.tempdir, this_filter_name, '{}.fasta'.format(subset_name))
-                subset_sortbysize_fasta = os.path.join(self.tempdir, this_filter_name,
-                                                       '{}_sortbysize.fasta'.format(subset_name))
-
-                ###################################################################
-                # 2. Sort variants by abundance
-                ###################################################################
-                with open(subset_fasta, 'w') as fout:
-                    variant_sequence_list = []
-                    for variant_id in variant_id_list:
-                        # with engine.connect() as conn:
-                        #     stmt = select([self.variant_df.__table__.c.sequence]).where(
-                            df = self.variant_df.loc[(self.variant_df.id == variant_id)]
-                            variant_sequence = df.sequence
-                            variant_sequence_list.append(variant_sequence)
-                            fout.write(">{}\n{}\n".format(variant_id, variant_sequence))
-                            del df
-                vsearch_sortbysize_args = {"sortbysize": subset_fasta, "output": subset_sortbysize_fasta}
-                vsearch_sortbysize = Vsearch2(**vsearch_sortbysize_args)
-                vsearch_sortbysize.run()
-
-                ###################################################################
-                # 3. Run uchime_denovo
-                ###################################################################
-                subset_borderline_fasta = os.path.join(self.tempdir, this_filter_name,'{}_borderline.fasta'.format(subset_name))
-                subset_nonchimeras_fasta = os.path.join(self.tempdir, this_filter_name,'{}_nonchimeras.fasta'.format(subset_name))
-                subset_chimeras_fasta = os.path.join(self.tempdir, this_filter_name,'{}_chimeras.fasta'.format(subset_name))
-                vsearch_chimera_args = {
-                    "uchime_denovo": subset_sortbysize_fasta,
-                    "borderline": subset_borderline_fasta,
-                    "nonchimeras": subset_nonchimeras_fasta,
-                    "chimeras": subset_chimeras_fasta
-                }
-                vsearch_chimera = Vsearch3(**vsearch_chimera_args)
-                vsearch_chimera.run()
-
-                ###################################################################
-                # 4. Delete variant from replicate/sample if chimeras
-                ###################################################################
-                subset_chimera_seqio = SeqIO.parse(open(subset_chimeras_fasta), 'fasta')
-                for subset_chimera in subset_chimera_seqio:
-                    self.variant_read_count_df.loc[variant_id, 'passed'] = False
-                    self.variant_read_count_df.loc[subset_chimera.id, 'f10_chimera'] = False  # does not pass
-        import pdb;
-        pdb.set_trace()
-
+            ###################################################################
+            #
+            # 4. Delete variant from replicate/sample if chimeras
+            #
+            ###################################################################
+            with open(chimear2_chimeras_fasta) as fasta_fin:
+                chimera_seqio = SeqIO.parse(chimear2_chimeras_fasta, 'fasta')
+                for chimera_seqrecord in chimera_seqio:
+                    variant_id = int(chimera_seqrecord.id.split(';')[0])
+                    filter_output_df.loc[(filter_output_df['run_id'] == run_id)
+                                         & (filter_output_df['marker_id'] == marker_id)
+                                         & (filter_output_df['biosample_id'] == biosample_id)
+                                         & (filter_output_df['variant_id'] == variant_id), 'filter_delete'] = 1
+        #
+        self.assertTrue(filter_output_df.loc[(filter_output_df.run_id == 1)
+                                                                         & (filter_output_df.marker_id == 1)
+                                                                         & (filter_output_df.variant_id == 6)
+                                                                         & (filter_output_df.biosample_id == 1)
+                                                                         & (filter_output_df.replicate_id == 1)
+                                                                         & (filter_output_df.filter_id == 12),
+                                                                        'filter_delete'].values[0])
+        #
+        self.assertTrue(not filter_output_df.loc[(filter_output_df.run_id == 1)
+                                                                         & (filter_output_df.marker_id == 1)
+                                                                         & (filter_output_df.variant_id == 1)
+                                                                         & (filter_output_df.biosample_id == 1)
+                                                                         & (filter_output_df.replicate_id == 1)
+                                                                         & (filter_output_df.filter_id == 12),
+                                                                        'filter_delete'].values[0])
