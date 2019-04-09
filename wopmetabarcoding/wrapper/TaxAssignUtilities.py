a4f3fdee9ea3229c0e1f24625690f4f1f901df40
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-04-09 16:08:16 +0200

    TST TaxAssign in test

--- wopmetabarcoding/wrapper/TaxAssignUtilities.py
+++ wopmetabarcoding/wrapper/TaxAssignUtilities.py
@@ -23,6 +23,14 @@ def taxonomy_sqlite_to_df(taxonomy_sqlite):
     con.close()
     return taxonomy_db_df
 
+def f02_import_blast_output_into_df(blast_output_tsv_path):
+    blast_out_df = pandas.read_csv(blast_output_tsv_path, sep="\t", header=None, names=['target_id', 'identity', 'target_tax_id'], usecols=[1, 2, 5])
+    # extract the target_id
+    blast_out_df.target_id = blast_out_df.target_id.str.split('|', 2).str[1]
+    blast_out_df.target_id = pandas.to_numeric(blast_out_df.target_id)
+    return blast_out_df
+
+
 def tax_id_to_taxonomy_lineage(tax_id, taxonomy_db_df):
     """
     Takes tax_id and taxonomy_db.sqlite DB and create a dictionary with the taxonomy lineage
@@ -48,9 +56,35 @@ def tax_id_to_taxonomy_lineage(tax_id, taxonomy_db_df):
         tax_id = parent_tax_id
     return lineage_dic
 
+def f04_blast_result_subset(blast_result_subset_df, taxonomy_db_df):
+    """
+    Takes blast_result_subset_df and returns tax_lineage_df with lineages of each target_tax_id
 
+    Args:
+        blast_result_subset_df (DataFrame): DataFrame with result subset for given identity
+            where each column is target_id and target_tax_id
+    target_id  target_tax_id
+0  1049499563         761875
+1  1049496963         761875
+        taxonomy_db_df (pandas.DataFrame: DataFrame with taxonomy information
 
-def select_ltg(tax_lineage_df, identity, identity_threshold=97, include_prop=90, min_number_of_taxa=3):
+    Returns:
+        DataFrame: with a lineage per row that corresponds to the the lineage of each taxon from the blast sequence targets
+   class  cohort  family  genus  infraclass  kingdom  no rank  order  phylum  species  subclass  subfamily  suborder  subphylum  superfamily  superkingdom  superorder
+0  50557   33392   41030  50443       33340    33208   131567  30263    6656   761875      7496     147297     93873       6960        41029          2759       85604
+1  50557   33392   41030  50443       33340    33208   131567  30263    6656   761875      7496     147297     93873       6960        41029          2759       85604
+
+    """
+    lineage_list = []
+    for target_tax_id in blast_result_subset_df.target_tax_id.unique().tolist():
+        lineage_list.append(tax_id_to_taxonomy_lineage(target_tax_id, taxonomy_db_df))
+    tax_lineage_df = pandas.DataFrame(lineage_list)
+    tax_lineage_df = blast_result_subset_df.merge(tax_lineage_df, left_on='target_tax_id', right_on='tax_id')
+    tax_lineage_df.drop('target_id', axis=1, inplace=True)
+    tax_lineage_df.drop('target_tax_id', axis=1, inplace=True)
+    return tax_lineage_df
+
+def f05_select_ltg(tax_lineage_df, identity, identity_threshold=97, include_prop=90, min_number_of_taxa=3):
     """
     Given tax_lineage_df, selects the LTG
 
@@ -72,7 +106,7 @@ def select_ltg(tax_lineage_df, identity, identity_threshold=97, include_prop=90,
         {'putative_ltg_id': tax_lineage_df.apply(lambda x: x.value_counts().index[0], axis=0),
          'putative_ltg_count': tax_lineage_df.apply(lambda x: x.value_counts().iloc[0], axis=0)})
     if identity >= identity_threshold: # rule for include_prop
-        putative_ltg_df['putative_ltg_percentage'] = putative_ltg_df.putative_ltg_count / putative_ltg_df.shape[0] * 100
+        putative_ltg_df['putative_ltg_percentage'] = putative_ltg_df.putative_ltg_count / tax_lineage_df.shape[0] * 100
         ltg_tax_id = putative_ltg_df.loc[putative_ltg_df.putative_ltg_percentage >= include_prop, 'putative_ltg_id'].tail(1).values[0]
         ltg_rank = putative_ltg_df.loc[putative_ltg_df.putative_ltg_percentage >= include_prop, 'putative_ltg_id'].index[-1]
     else: # rule for min_number_of_taxa
