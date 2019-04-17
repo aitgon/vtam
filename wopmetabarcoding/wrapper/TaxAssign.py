08a7a1c9017f09437a2d19ba2ab1ff1bb2541beb
Reda Mekdad <mekdadr6@gmail.com>
2019-04-17 10:00:22 +0200

    PROCESSING t3
    Former-commit-id: 9b731412783de013f61c3bc2b1b59a8038537ab1

--- wopmetabarcoding/wrapper/TaxAssign.py
+++ wopmetabarcoding/wrapper/TaxAssign.py
@@ -1,9 +1,17 @@
+import errno
+import inspect
+import os
+
 import sqlalchemy
+from Bio.Blast import NCBIWWW
 from wopmars.framework.database.tables.ToolWrapper import ToolWrapper
 from wopmars.utils.Logger import Logger
-
+from wopmetabarcoding.utils.constants import data_dir
 from sqlalchemy import select
 import pandas
+
+from repositories.wopmetabarcodin.wopmetabarcoding.utils import logger
+from repositories.wopmetabarcodin.wopmetabarcoding.utils.constants import tempdir
 
 
 class TaxAssign(ToolWrapper):
@@ -82,9 +90,6 @@ class TaxAssign(ToolWrapper):
                                                               columns=['marker_id', 'run_id', 'variant_id',
                                                                        'biosample_id', 'replicate_id', 'read_count'])
 
-        import pdb;
-        pdb.set_trace()
-
         # Select id/ sequence from variant table
         variant_model_table = variant_model.__table__
         stmt_variant = select([variant_model_table.c.id,
@@ -102,26 +107,55 @@ class TaxAssign(ToolWrapper):
         variant_passed_df = variant_df.loc[codon_stop_df['variant_id']]
 
         # creation one fasta file containing all the variant
-
-        ofile = open('variant_passed.fasta', "w") # TODO Fasta goes to TMP
+        try:
+            os.makedirs(data_dir)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+        variant_paased_fasta = os.path.join(data_dir, 'variant_passed.fasta')
+        ofile = open(variant_paased_fasta, "w")          # TODO Fasta goes to TMP  /done
         for i in range(0, len(variant_df)):
             df = variant_passed_df.iloc[i]
             ofile.write(">" + str(df['id']) + "\n" + str(df['sequence']) + "\n")
         #
         ofile.close()
         #
+
         ##########################################################
         #
-        # Run qblast: test_f06_2_run_qblast
+        # 2- Run qblast: test_f06_2_run_qblast
         #
         ##########################################################
-        import pdb;
-        pdb.set_trace()
+
+        # Run and read blast result
+        with open(variant_paased_fasta) as fin:
+            variant_fasta_content = fin
+
+            logger.debug(
+                "file: {}; line: {}; Blasting...".format(__file__, inspect.currentframe().f_lineno))
+            result_handle = NCBIWWW.qblast("blastn", "nt", variant_fasta_content.read(), format_type='Tabular')
+            blast_result_tsv = os.path.join(data_dir, "tax_assign_blast.tsv")
+            with open(blast_result_tsv, 'w') as out_handle:
+                out_handle.write(result_handle.read())
+            result_handle.close()
+        blast_result_df = pandas.read_csv(blast_result_tsv, sep="\t", skiprows=13, usecols=[0, 1, 2],
+                                          header=None, names=['variant_id', 'gb_accession', 'identity'])
+
+        # let only the output coantaining non null values
+        blast_varaint_result_df = blast_result_df[blast_result_df['identity'].notnull()].copy()
+
+        ##########################################################
+        #
+        # 3- test_f06_3_annotate_blast_output_with_tax_id & test_f03_import_blast_output_into_df
+        #
+        ##########################################################
+
+
         ##########################################################
         #
         # Previous preparation -------------------
-        # bin/create_db_taxonomy (sqlite)
-        # bin/create_db_accession_to_tax_id (sqlite)
+        # bin/create_db_taxonomy (sqlite) / done
+        # bin/create_db_accession_to_tax_id (sqlite) / done  (do result sqlite file  need to stell in tmp directory?)
         #
         # This wrapper -------------------
         # 1 Create Fasta from Variants that passed the Filters: One FASTA for all variants
@@ -133,6 +167,5 @@ class TaxAssign(ToolWrapper):
         ##########################################################
 
 
-        # creation fasta file from the variant that passed the filter
-        #  take the id of variant that passed all the filter and get from the table variant the sequence equivalent
+
 
