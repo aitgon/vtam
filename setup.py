8726c4e3992f2e4242cb6920559f80c7511356ef
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-04-17 10:49:39 +0200

    ENH

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     description="Metabarcoding wrappers and models for WopMars",
     install_requires=install_requires,
     entry_points={
-        #'console_scripts': ['create_db_taxonomy=bin.create_db_taxonomy:main']
-        'console_scripts': ['create_accession2taxid=bin.create_accession2taxid_db:main']                        
+        'console_scripts': ['create_db_taxonomy=bin.create_db_taxonomy:main',
+                            'create_db_accession2taxid=bin.create_db_accession2taxid:main']
     },
     )
 
