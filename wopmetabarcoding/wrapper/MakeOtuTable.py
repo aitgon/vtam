d607a2b18b37185b58ab0fb2a2addb217770d99c
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-05-15 11:49:41 +0200

    BUG MakeOtuTable
    Former-commit-id: d8f69ab5df0b28fe0496d660e98a91e9adf3736c

--- wopmetabarcoding/wrapper/MakeOtuTable.py
+++ wopmetabarcoding/wrapper/MakeOtuTable.py
@@ -157,13 +157,13 @@ class MakeOtuTable(ToolWrapper):
 
         filter_codon_stop_model_table = filter_codon_stop_model.__table__
         variant_model_table = variant_model.__table__
-        stmt_variant = select([filter_codon_stop_model_table.c.variant_id, variant_model_table.c.sequence]) \
+        stmt_filter_codon_stop = select([filter_codon_stop_model_table.c.variant_id, variant_model_table.c.sequence]) \
             .where(filter_codon_stop_model_table.c.variant_id == variant_model_table.c.id) \
             .where(filter_codon_stop_model_table.c.filter_delete == 0).distinct().order_by("variant_id")
         # Select to DataFrame
         variant_list = []
         with engine.connect() as conn:
-            for row in conn.execute(stmt_variant).fetchall():
+            for row in conn.execute(stmt_filter_codon_stop).fetchall():
                 variant_list.append(row)
         variant_df = pandas.DataFrame.from_records(variant_list, columns=['variant_id', 'variant_sequence'])
 
@@ -235,7 +235,7 @@ class MakeOtuTable(ToolWrapper):
 
         tax_assign_model_table = tax_assign_model.__table__
         stmt_ltg_tax_assign = select([tax_assign_model_table.c.variant_id,
-                               tax_assign_model_table.c.variant_id,
+                               tax_assign_model_table.c.identiy,
                                tax_assign_model_table.c.ltg_rank,
                                tax_assign_model_table.c.ltg_tax_id])
 
@@ -245,7 +245,7 @@ class MakeOtuTable(ToolWrapper):
             for row in conn.execute(stmt_ltg_tax_assign).fetchall():
                 tax_assign_list.append(row)
         ltg_tax_assign_df = pandas.DataFrame.from_records(run_list,
-                                                          columns=['variant_id', 'variant_id','ltg_rank','ltg_tax_id'])
+                                                          columns=['variant_id', 'identity','ltg_rank','ltg_tax_id'])
 
         #####
         #
@@ -254,19 +254,18 @@ class MakeOtuTable(ToolWrapper):
         #####
 
         filter_codon_stop_model_table = filter_codon_stop_model.__table__
-        stmt_variant = select([filter_codon_stop_model_table.c.run_id,
+        stmt_filter_codon_stop = select([filter_codon_stop_model_table.c.run_id,
                                filter_codon_stop_model_table.c.marker_id,
                                filter_codon_stop_model_table.c.variant_id,
                                filter_codon_stop_model_table.c.biosample_id,
                                filter_codon_stop_model_table.c.replicate_id,
                                filter_codon_stop_model_table.c.read_count,
-                               filter_codon_stop_model_table.c.fiter_id,
                                filter_codon_stop_model_table.c.filter_delete])\
             .where(filter_codon_stop_model_table.c.filter_delete == 0).distinct().order_by("variant_id")
         # Select to DataFrame
         filter_codon_stop_list = []
         with engine.connect() as conn:
-            for row in conn.execute(stmt_variant).fetchall():
+            for row in conn.execute(stmt_filter_codon_stop).fetchall():
                 filter_codon_stop_list.append(row)
 
         filter_codon_stop_df = pandas.DataFrame.from_records(variant_list, columns=['variant_id', 'variant_sequence'])
@@ -291,15 +290,9 @@ class MakeOtuTable(ToolWrapper):
         #####
 
 
-        Index = ['variant_id','run_name', 'marker_name', 'Tpos1_prerun', 'Tpos2_prerun', 'identity',
-               'ltg_rank', 'ltg_tax_id', 'phylum', 'class', 'order', 'family', 'genus',
-               'species','sequence_length', 'read_count','variant_sequence',]
-
-
 
         otu_df = f16_otu_table_maker(run_df, marker_df, variant_df, biosample_df, filter_codon_stop_df, ltg_tax_assign_df,
                             taxonomy_db_df)
-        otu_df = otu_df[Index]
 
         otu_df.to_csv(otu_table_tsv_path, sep='\t', index=False, header=True)
 
@@ -364,12 +357,6 @@ def f16_otu_table_maker(run_df,marker_df,variant_df,biosample_df,filter_codon_st
     # Merge ltg tax assign results
     # import pdb; pdb.set_trace()
     otu_df = otu_df.merge(ltg_tax_assign_df, on='variant_id')
-
-    ##########################
-    #
-    # Create lineage from each ltg_tax_id
-    #
-    ##########################
     list_lineage = []
     for tax_id in otu_df['ltg_tax_id'].tolist():
         dic_lineage = f04_1_tax_id_to_taxonomy_lineage(tax_id, taxonomy_db_df, give_tax_name=True)
@@ -381,5 +368,28 @@ def f16_otu_table_maker(run_df,marker_df,variant_df,biosample_df,filter_codon_st
     # Merge Otu_df with the lineage df
     otu_df = otu_df.merge(lineage_df, left_on='ltg_tax_id', right_on='tax_id')
     otu_df.drop('tax_id', axis=1, inplace=True)
+
+
+
+    ##########################
+    #
+    # Reorganize columns
+    #
+    ##########################
+    otu_df_columns = otu_df.columns.tolist()
+    # move items to given positions
+    otu_df_columns.insert(1, otu_df_columns.pop(otu_df_columns.index('marker_name')))
+    otu_df_columns.insert(2, otu_df_columns.pop(otu_df_columns.index('run_name')))
+    otu_df_columns.insert(3, otu_df_columns.pop(otu_df_columns.index('sequence_length')))
+    otu_df_columns.insert(4, otu_df_columns.pop(otu_df_columns.index('read_count')))
+    #
+    # append at the end
+    otu_df_columns = otu_df_columns + [otu_df_columns.pop(otu_df_columns.index('ltg_tax_id'))]
+    otu_df_columns = otu_df_columns + [otu_df_columns.pop(otu_df_columns.index('identity'))]
+    otu_df_columns = otu_df_columns + [otu_df_columns.pop(otu_df_columns.index('ltg_rank'))]
+    otu_df_columns = otu_df_columns + [otu_df_columns.pop(otu_df_columns.index('variant_sequence'))]
+    #
+    # reorder
+    otu_df = otu_df[otu_df_columns]
 
     return otu_df
