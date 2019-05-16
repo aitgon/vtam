2039138a66be86ae8868bc1ca6c0b5846efca4b4
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-05-16 09:16:19 +0200

    BUG Fix make otu table
    Former-commit-id: 51e47ceee3962b180d68fcb8fffb0ce540f83579

--- wopmetabarcoding/wrapper/MakeOtuTable.py
+++ wopmetabarcoding/wrapper/MakeOtuTable.py
@@ -87,35 +87,35 @@ class MakeOtuTable(ToolWrapper):
         # Options
 
 
-        ##########################################################
-        #
-        # 2. Read sample2fasta to get run_id, marker_id, biosample_id, replicate_id for current analysis
-        #
-        ##########################################################
-        sample2fasta_df = pandas.read_csv(input_file_sample2fasta, sep="\t", header=None,\
-            names=['tag_forward', 'primer_forward', 'tag_reverse', 'primer_reverse', 'marker_name', 'biosample_name',\
-            'replicate_name', 'run_name', 'fastq_fwd', 'fastq_rev', 'fasta'])
-        sample_instance_list = []
-        for row in sample2fasta_df.itertuples():
-            marker_name = row.marker_name
-            run_name = row.run_name
-            biosample_name = row.biosample_name
-            replicate_name = row.replicate_name
-            with engine.connect() as conn:
-                # get run_id ###########
-                stmt_select_run_id = select([run_model.__table__.c.id]).where(run_model.__table__.c.name==run_name)
-                run_id = conn.execute(stmt_select_run_id).first()[0]
-                # get marker_id ###########
-                stmt_select_marker_id = select([marker_model.__table__.c.id]).where(marker_model.__table__.c.name==marker_name)
-                marker_id = conn.execute(stmt_select_marker_id).first()[0]
-                # get biosample_id ###########
-                stmt_select_biosample_id = select([biosample_model.__table__.c.id]).where(biosample_model.__table__.c.name==biosample_name)
-                biosample_id = conn.execute(stmt_select_biosample_id).first()[0]
-                # get replicate_id ###########
-                stmt_select_replicate_id = select([replicate_model.__table__.c.id]).where(replicate_model.__table__.c.name==replicate_name)
-                replicate_id = conn.execute(stmt_select_replicate_id).first()[0]
-                # add this sample_instance ###########
-                sample_instance_list.append({'run_id': run_id, 'marker_id': marker_id, 'biosample_id':biosample_id, 'replicate_id':replicate_id})
+        # ##########################################################
+        # #
+        # # 2. Read sample2fasta to get run_id, marker_id, biosample_id, replicate_id for current analysis
+        # #
+        # ##########################################################
+        # sample2fasta_df = pandas.read_csv(input_file_sample2fasta, sep="\t", header=None,\
+        #     names=['tag_forward', 'primer_forward', 'tag_reverse', 'primer_reverse', 'marker_name', 'biosample_name',\
+        #     'replicate_name', 'run_name', 'fastq_fwd', 'fastq_rev', 'fasta'])
+        # sample_instance_list = []
+        # for row in sample2fasta_df.itertuples():
+        #     marker_name = row.marker_name
+        #     run_name = row.run_name
+        #     biosample_name = row.biosample_name
+        #     replicate_name = row.replicate_name
+        #     with engine.connect() as conn:
+        #         # get run_id ###########
+        #         stmt_select_run_id = select([run_model.__table__.c.id]).where(run_model.__table__.c.name==run_name)
+        #         run_id = conn.execute(stmt_select_run_id).first()[0]
+        #         # get marker_id ###########
+        #         stmt_select_marker_id = select([marker_model.__table__.c.id]).where(marker_model.__table__.c.name==marker_name)
+        #         marker_id = conn.execute(stmt_select_marker_id).first()[0]
+        #         # get biosample_id ###########
+        #         stmt_select_biosample_id = select([biosample_model.__table__.c.id]).where(biosample_model.__table__.c.name==biosample_name)
+        #         biosample_id = conn.execute(stmt_select_biosample_id).first()[0]
+        #         # get replicate_id ###########
+        #         stmt_select_replicate_id = select([replicate_model.__table__.c.id]).where(replicate_model.__table__.c.name==replicate_name)
+        #         replicate_id = conn.execute(stmt_select_replicate_id).first()[0]
+        #         # add this sample_instance ###########
+        #         sample_instance_list.append({'run_id': run_id, 'marker_id': marker_id, 'biosample_id':biosample_id, 'replicate_id':replicate_id})
 
         ##########################################################
         #
@@ -126,25 +126,25 @@ class MakeOtuTable(ToolWrapper):
         #
         # 3a. Select variants from this run/markerbiosample/replicate combination
         #
-        variant_id_delete_list = []
-        for sample_instance in sample_instance_list:
-            stmt_select_variant_id_delete = select([filter_codon_stop_model.__table__.c.variant_id])\
-                .where(filter_codon_stop_model.__table__.c.run_id == sample_instance['run_id'])\
-                .where(filter_codon_stop_model.__table__.c.marker_id == sample_instance['marker_id'])\
-                .where(filter_codon_stop_model.__table__.c.biosample_id == sample_instance['biosample_id'])\
-                .where(filter_codon_stop_model.__table__.c.replicate_id == sample_instance['replicate_id'])
-            # Select to DataFrame
-            with engine.connect() as conn:
-                for row in conn.execute(stmt_select_variant_id_delete).fetchall():
-                    variant_id = row[0]
-                    if not variant_id in variant_id_delete_list:
-                        variant_id_delete_list.append(row)
-        #
-        # 3b. Delete variants from this run/markerbiosample/replicate combination
-        #
-        variant_instance_list = [{'variant_id': variant_id} for variant_id in variant_id_delete_list]
-        with engine.connect() as conn:
-            conn.execute(tax_assign_model.__table__.delete(), variant_instance_list)
+        # variant_id_delete_list = []
+        # for sample_instance in sample_instance_list:
+        #     stmt_select_variant_id_delete = select([filter_codon_stop_model.__table__.c.variant_id])\
+        #         .where(filter_codon_stop_model.__table__.c.run_id == sample_instance['run_id'])\
+        #         .where(filter_codon_stop_model.__table__.c.marker_id == sample_instance['marker_id'])\
+        #         .where(filter_codon_stop_model.__table__.c.biosample_id == sample_instance['biosample_id'])\
+        #         .where(filter_codon_stop_model.__table__.c.replicate_id == sample_instance['replicate_id'])
+        #     # Select to DataFrame
+        #     with engine.connect() as conn:
+        #         for row in conn.execute(stmt_select_variant_id_delete).fetchall():
+        #             variant_id = row[0]
+        #             if not variant_id in variant_id_delete_list:
+        #                 variant_id_delete_list.append(row)
+        # #
+        # # 3b. Delete variants from this run/markerbiosample/replicate combination
+        # #
+        # variant_instance_list = [{'variant_id': variant_id} for variant_id in variant_id_delete_list]
+        # with engine.connect() as conn:
+        #     conn.execute(tax_assign_model.__table__.delete(), variant_instance_list)
 
         #
         ##########################################################
@@ -173,11 +173,11 @@ class MakeOtuTable(ToolWrapper):
         #
         ##########################################################
 
-         #####
-         #
-         #table run
-         #
-         #####
+        #####
+        #
+        # table run
+        #
+        #####
         run_model_table = run_model.__table__
         stmt_run = select([run_model_table.c.id,
                            run_model_table.c.name])
@@ -200,11 +200,11 @@ class MakeOtuTable(ToolWrapper):
                                  biosample_model_table.c.name])
 
         # Select to DataFrame
-        run_list = []
+        biosample_list = []
         with engine.connect() as conn:
             for row in conn.execute(stmt_biosample).fetchall():
-                run_list.append(row)
-        biosample_df = pandas.DataFrame.from_records(run_list,
+                biosample_list.append(row)
+        biosample_df = pandas.DataFrame.from_records(biosample_list,
                                                      columns=['id', 'name'])
 
 
@@ -213,17 +213,16 @@ class MakeOtuTable(ToolWrapper):
         # table marker
         #
         #####
-
         marker_model_table = marker_model.__table__
         stmt_marker = select([marker_model_table.c.id,
                               marker_model_table.c.name])
 
-         # Select to DataFrame
+        # Select to DataFrame
         marker_list = []
         with engine.connect() as conn:
             for row in conn.execute(stmt_marker).fetchall():
                 marker_list.append(row)
-        marker_df = pandas.DataFrame.from_records(run_list,
+        marker_df = pandas.DataFrame.from_records(marker_list,
                                                   columns=['id', 'name'])
 
 
@@ -244,7 +243,7 @@ class MakeOtuTable(ToolWrapper):
         with engine.connect() as conn:
             for row in conn.execute(stmt_ltg_tax_assign).fetchall():
                 tax_assign_list.append(row)
-        ltg_tax_assign_df = pandas.DataFrame.from_records(run_list,
+        ltg_tax_assign_df = pandas.DataFrame.from_records(tax_assign_list,
                                                           columns=['variant_id', 'identity','ltg_rank','ltg_tax_id'])
 
         #####
@@ -259,16 +258,14 @@ class MakeOtuTable(ToolWrapper):
                                filter_codon_stop_model_table.c.variant_id,
                                filter_codon_stop_model_table.c.biosample_id,
                                filter_codon_stop_model_table.c.replicate_id,
-                               filter_codon_stop_model_table.c.read_count,
-                               filter_codon_stop_model_table.c.filter_delete])\
+                               filter_codon_stop_model_table.c.read_count])\
             .where(filter_codon_stop_model_table.c.filter_delete == 0).distinct().order_by("variant_id")
         # Select to DataFrame
         filter_codon_stop_list = []
         with engine.connect() as conn:
             for row in conn.execute(stmt_filter_codon_stop).fetchall():
                 filter_codon_stop_list.append(row)
-
-        filter_codon_stop_df = pandas.DataFrame.from_records(variant_list, columns=['variant_id', 'variant_sequence'])
+        filter_codon_stop_df = pandas.DataFrame.from_records(filter_codon_stop_list, columns=['run_id', 'marker_id', 'variant_id', 'biosample_id', 'replicate_id', 'read_count'])
 
         #####
         #
@@ -344,7 +341,7 @@ def f16_otu_table_maker(run_df,marker_df,variant_df,biosample_df,filter_codon_st
     # Pivot biosamples
     otu_biosamples_df = otu_biosamples_df.pivot_table(index=['run_name', 'marker_name', 'variant_id'],
                                                       columns="biosample_name",
-                                                      values='read_count').reset_index()
+                                                      values='read_count', fill_value=0).reset_index()
     #
     ############################
     #
@@ -355,20 +352,19 @@ def f16_otu_table_maker(run_df,marker_df,variant_df,biosample_df,filter_codon_st
     #
     #
     # Merge ltg tax assign results
-    # import pdb; pdb.set_trace()
     otu_df = otu_df.merge(ltg_tax_assign_df, on='variant_id')
     list_lineage = []
-    for tax_id in otu_df['ltg_tax_id'].tolist():
+    for tax_id in otu_df['ltg_tax_id'].unique().tolist():
         dic_lineage = f04_1_tax_id_to_taxonomy_lineage(tax_id, taxonomy_db_df, give_tax_name=True)
         list_lineage.append(dic_lineage)
     lineage_df = pandas.DataFrame(data=list_lineage)
     lineage_list_df_columns_sorted = list(filter(lambda x: x in lineage_df.columns.tolist(), rank_hierarchy_otu_table))
-    lineage_df = lineage_df[lineage_list_df_columns_sorted + ['tax_id']]
+    lineage_list_df_columns_sorted = lineage_list_df_columns_sorted + ['tax_id']
+    lineage_df = lineage_df[lineage_list_df_columns_sorted]
 
     # Merge Otu_df with the lineage df
     otu_df = otu_df.merge(lineage_df, left_on='ltg_tax_id', right_on='tax_id')
     otu_df.drop('tax_id', axis=1, inplace=True)
-
 
 
     ##########################
