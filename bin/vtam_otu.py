7d4f9803374f34c6bddffd2e4061a67ed6812301
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-07-19 10:47:50 +0200

    ENH variant threshold specific
    Former-commit-id: 12bae8aba2cb294cffc3188c30bc45e6428970b1

--- bin/vtam_otu.py
+++ bin/vtam_otu.py
@@ -8,17 +8,12 @@ import sys
 import jinja2
 import yaml
 import os
-# import subprocess
 
-from wopmetabarcoding.utils.constants import tempdir
 from wopmetabarcoding.utils.logger import logger
 
 
 def vtam_run(args_dic):
     #
-    wopfile_file_name = 'Wopfile_otu.yml'
-    wopfile_in_path = os.path.join(os.path.dirname(__file__), '../data', wopfile_file_name)
-    wopfile_out_path = os.path.join(tempdir, wopfile_file_name)
     #
     #############################################################
     #
@@ -32,11 +27,20 @@ def vtam_run(args_dic):
     # Render Wopfile
     #
     #############################################################
-    with open(wopfile_in_path) as fin:
-        template = jinja2.Template(fin.read())
-    wopfile_rendered = template.render(args_dic)
+    #
+    wopfile_file_name = 'Wopfile_otu.yml'
+    template_dir = os.path.join(os.path.dirname(__file__), '../data')
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+    if 'threshold_specific' in args_dic: # threshold variant specific
+        template = env.get_template('Wopfile_otu_thresholdspecific.yml')
+    else:
+        template = env.get_template('Wopfile_otu_thresholdgeneral.yml')
+    rendered_content = template.render(args_dic)
+    #
+    # Write to outdir
+    wopfile_out_path = os.path.join(args_dic['outdir'], wopfile_file_name)
     with open(wopfile_out_path, "w") as fout:
-        fout.write(wopfile_rendered)
+        fout.write(rendered_content)
     args_dic['wopfile_out_path'] = wopfile_out_path
     #
     logger.debug(
@@ -54,13 +58,9 @@ def vtam_run(args_dic):
         cmd = cmd + " -n"
     if args_dic['forceall']:
         cmd = cmd + " -F"
-    if 'targetrule' is args_dic:
+    if 'targetrule' in args_dic:
         cmd = cmd + " -t {targetrule}".format(**args_dic)
-    # import pdb; pdb.set_trace()
-    # p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     os.system(cmd)
-    # stdout = p.communicate()
-    # p.wait()  # wait program to finish
     sys.exit(0)
 
 def create_parser():
@@ -71,6 +71,7 @@ def create_parser():
     parser.add_argument('--outdir', nargs=1, help="Directory for output", required=True)
     parser.add_argument('--params', nargs=1, help="YML file with parameter values", required=True)
     parser.add_argument('--filter_lfn_variant', nargs=1, help="Boolean 0|1 to filter_lfn_variant (1) or filter_lfn_variant_replicate (0)", required=True)
+    parser.add_argument('--threshold_specific', nargs=1, help="Variant or variant-replicate specific threshold", required=False)
     parser.add_argument('-F', '--forceall', action='store_true', help="Force argument of WopMars", required=False)
     parser.add_argument('-t', '--targetrule', nargs=1, help="Execute the workflow to the given RULE: SampleInformation, ...", required=False)
     parser.add_argument('-n', '--dry-run', dest='dryrun', action='store_true', help="Only display what would have been done.")
@@ -87,13 +88,15 @@ def main():
         'outdir': args.outdir[0],
         'filter_lfn_variant': args.filter_lfn_variant[0],
         'params': args.params[0],
-        'forceall': args.forceall,
         'sortreads': os.path.join(args.outdir[0], 'sortreads.tsv'),
         'outtable': os.path.join(args.outdir[0], 'otutable.tsv'),
+        'forceall': args.forceall,
         'dryrun': args.dryrun,
     }
     if not args.targetrule is None:
         args_dic['targetrule'] = args.targetrule[0]
+    if not args.threshold_specific is None:
+        args_dic['threshold_specific'] = args.threshold_specific[0]
     vtam_run(args_dic)
 
 if __name__=='__main__':
