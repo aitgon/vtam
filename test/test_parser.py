9e38728ff0b6546eaa808b46d0f1aeecfc39c2c0
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-09-23 18:32:12 +0200

    ENH subparser
    Former-commit-id: d4819401929b9a39687ae0530847033f9b369252

--- test/test_parser.py
+++ test/test_parser.py
@@ -11,52 +11,60 @@ class TestParser(TestCase):
     def setUp(self):
         pass
 
-    def test_parser_base(self):
-        args_str = 'command --db test '
-        #
-        # Base parser
-        parser = ArgParser.get_arg_parser_base()
-        args = parser.parse_args(args_str.split())
-        self.assertTrue(hasattr(args, 'db'))
+    # def test_parser_base(self):
+    #     args_str = 'merge --db test '
+    #     #
+    #     # Base parser
+    #     parser = ArgParser.get_arg_parser_base()
+    #     args = parser.parse_args(args_str.split())
+    #     self.assertTrue(hasattr(args, 'db'))
 
     def test_parser_merge(self):
-        args_str = 'merge --db test --fastqinfo {} --fastqdir {} --fastainfo foo --fastadir foo'.format(__file__,
+        #
+        # Minimal merge command
+        args_str = 'merge --fastqinfo {} --fastqdir {} --fastainfo foo --fastadir foo'.format(__file__,
                 os.path.dirname(__file__))
-        #
-        # Base parser
+        # args_str = 'merge --db test -n'
         parser = ArgParser.get_arg_parser_merge()
-        args = parser.parse_args(args_str.split())
-        self.assertTrue(hasattr(args, 'fastqinfo'))
-
-    def test_cli(self):
-        args_str = 'vtam merge --db test --fastqinfo {} --fastqdir {} --fastainfo foo --fastadir foo'.format(__file__,
-                                                                                                  os.path.dirname(
-                                                                                                      __file__))
-        VTAM(args_str.split())
-
-    def test_1(self):
-        OptionManager.instance()['test2'] = 'test2'
-        print(OptionManager.instance())
-        OptionManager.instance()['test'] = 'test3'
-        print(OptionManager.instance())
-
-    def test_merge_args_to_optionsmanager(self):
-        args_str = 'merge --db test --fastqinfo {} --fastqdir {} --fastainfo foo --fastadir foo'.format(__file__,
+        parser.parse_args(args_str.split())
+        #
+        # Merge command with some wopmars arguments
+        args_str = 'merge --fastqinfo {} --fastqdir {} --fastainfo foo --fastadir foo -n'.format(__file__,
                 os.path.dirname(__file__))
-        #
-        # Base parser
+        # args_str = 'merge --db test -n'
         parser = ArgParser.get_arg_parser_merge()
-        args = parser.parse_args(args_str.split())
-        for k in vars(args):
-            if k is 'command':
-                OptionManager.instance()[k] = vars(args)[k]
-                continue
-            try:
-                OptionManager.instance()[k] = vars(args)[k][0]
-            except TypeError:
-                OptionManager.instance()[k] = vars(args)[k]
-        #
-        # assert
-        #
-        self.assertTrue(sorted(list(OptionManager.instance().keys())) == sorted(['command', 'dryrun', 'forceall',
-                'log', 'params', 'targetrule', 'verbose', 'db', 'fastqinfo', 'fastainfo', 'fastqdir', 'fastadir']))
+        parser.parse_args(args_str.split())
+
+    # 
+    # def test_cli(self):
+    #     args_str = 'vtam merge --db test --fastqinfo {} --fastqdir {} --fastainfo foo --fastadir foo'.format(__file__,
+    #                                                                                               os.path.dirname(
+    #                                                                                                   __file__))
+    #     VTAM(args_str.split())
+    # 
+    # def test_1(self):
+    #     OptionManager.instance()['test2'] = 'test2'
+    #     print(OptionManager.instance())
+    #     OptionManager.instance()['test'] = 'test3'
+    #     print(OptionManager.instance())
+    # 
+    # def test_merge_args_to_optionsmanager(self):
+    #     args_str = 'merge --db test --fastqinfo {} --fastqdir {} --fastainfo foo --fastadir foo'.format(__file__,
+    #             os.path.dirname(__file__))
+    #     #
+    #     # Base parser
+    #     parser = ArgParser.get_arg_parser_merge()
+    #     args = parser.parse_args(args_str.split())
+    #     for k in vars(args):
+    #         if k is 'command':
+    #             OptionManager.instance()[k] = vars(args)[k]
+    #             continue
+    #         try:
+    #             OptionManager.instance()[k] = vars(args)[k][0]
+    #         except TypeError:
+    #             OptionManager.instance()[k] = vars(args)[k]
+    #     #
+    #     # assert
+    #     #
+    #     self.assertTrue(sorted(list(OptionManager.instance().keys())) == sorted(['command', 'dryrun', 'forceall',
+    #             'log', 'params', 'targetrule', 'verbose', 'db', 'fastqinfo', 'fastainfo', 'fastqdir', 'fastadir']))
--- wopmetabarcoding/utils/ArgParser.py
+++ wopmetabarcoding/utils/ArgParser.py
@@ -8,6 +8,60 @@ from wopmetabarcoding.utils.PathFinder import PathFinder
 class ArgParser():
 
     @staticmethod
+    def get_arg_parser_merge():
+        # create the top-level parser
+        parser_vtam = argparse.ArgumentParser(add_help=False)
+
+        parser_vtam.add_argument('--db', dest='db', nargs=1, help="SQLITE file with DB", required=False,
+                            type=os.path.abspath)
+        parser_vtam.add_argument('--dry-run', '-n', dest='dryrun', action='store_true',
+                            help="Only display what would have been done.", required=False)
+        parser_vtam.add_argument('-F', '--forceall', dest='forceall', action='store_true', help="Force argument of WopMars",
+                            required=False)
+        parser_vtam.add_argument('--log', nargs=1, dest='log',
+                            help="Write log to file.", required=False,
+                            type=os.path.abspath)
+        parser_vtam.add_argument('--params', nargs=1, help="YML file with parameter values", required=False)
+        parser_vtam.add_argument('-t', '--targetrule', nargs=1, help="Execute the workflow to the given target "
+                                                                "RULE: SampleInformation, ...", required=False)
+        parser_vtam.add_argument('-v', dest='verbose', action='count',
+                            help="Set verbosity level, eg. None (Error level) -v (Info level) or -vv (Debug level)",
+                            required=False)
+        subparsers = parser_vtam.add_subparsers()
+
+        #############################################
+        #
+        # create the parser for the "merge" command
+        #
+        #############################################
+        parser_vtam_merge = subparsers.add_parser('merge', add_help=True, parents=[parser_vtam])
+        parser_vtam_merge.add_argument('--fastqinfo', dest='fastqinfo', nargs=1, help="TSV file with FASTQ sample information",
+                            required=True, type=lambda x:
+                            PathFinder.check_file_exists_and_is_nonempty(x, "Verify the '--fastqinfo' argument"))
+        parser_vtam_merge.add_argument('--fastainfo', dest='fastainfo', nargs=1, help="TSV file with FASTA sample information",
+                            required=True, type=os.path.abspath)
+        parser_vtam_merge.add_argument('--fastqdir', dest='fastqdir', nargs=1, help="Directory with FASTQ files", required=True,
+                            type=lambda x:
+                            PathFinder.check_dir_exists_and_is_nonempty(x, "Verify the '--fastqdir' argument"))
+        parser_vtam_merge.add_argument('--fastadir', dest='fastadir', nargs=1, help="Directory with FASTA files", required=True,
+                            type=os.path.abspath)
+
+
+        #############################################
+        #
+        # create the parser for the "otu" command
+        #
+        #############################################        # parser_b = subparsers.add_parser('b', help='b help')
+        # parser_b.add_argument('--baz', choices='XYZ', help='baz help')
+        #
+        # # parse some argument lists
+        # parser.parse_args(['a', '12'])
+        #
+        # parser.parse_args(['--foo', 'b', '--baz', 'Z'])
+        return parser_vtam
+
+
+    @staticmethod
     def get_arg_parser_base():
         """These are base VTAM arguments"""
         parser = argparse.ArgumentParser(
@@ -47,29 +101,29 @@ class ArgParser():
         return parser
 
 
-    @staticmethod
-    def get_arg_parser_merge():
-        """These are 'vtam merge' arguments"""
-        # Base parser
-        parser = ArgParser.get_arg_parser_base()
-        ###################
-        #
-        # required
-        #
-        ###################
-        parser.add_argument('--db', dest='db', nargs=1, help="SQLITE file with DB", required=True,
-                            type=os.path.abspath)
-        parser.add_argument('--fastqinfo', dest='fastqinfo', nargs=1, help="TSV file with FASTQ sample information",
-                            required=True, type=lambda x:
-                            PathFinder.check_file_exists_and_is_nonempty(x, "Verify the '--fastqinfo' argument"))
-        parser.add_argument('--fastainfo', dest='fastainfo', nargs=1, help="TSV file with FASTA sample information",
-                            required=True, type=os.path.abspath)
-        parser.add_argument('--fastqdir', dest='fastqdir', nargs=1, help="Directory with FASTQ files", required=True,
-                            type=lambda x:
-                            PathFinder.check_dir_exists_and_is_nonempty(x, "Verify the '--fastqdir' argument"))
-        parser.add_argument('--fastadir', dest='fastadir', nargs=1, help="Directory with FASTA files", required=True,
-                            type=os.path.abspath)
-        return parser
+    # @staticmethod
+    # def get_arg_parser_merge():
+    #     """These are 'vtam merge' arguments"""
+    #     # Base parser
+    #     parser = ArgParser.get_arg_parser_base()
+    #     ###################
+    #     #
+    #     # required
+    #     #
+    #     ###################
+    #     parser.add_argument('--db', dest='db', nargs=1, help="SQLITE file with DB", required=True,
+    #                         type=os.path.abspath)
+    #     parser.add_argument('--fastqinfo', dest='fastqinfo', nargs=1, help="TSV file with FASTQ sample information",
+    #                         required=True, type=lambda x:
+    #                         PathFinder.check_file_exists_and_is_nonempty(x, "Verify the '--fastqinfo' argument"))
+    #     parser.add_argument('--fastainfo', dest='fastainfo', nargs=1, help="TSV file with FASTA sample information",
+    #                         required=True, type=os.path.abspath)
+    #     parser.add_argument('--fastqdir', dest='fastqdir', nargs=1, help="Directory with FASTQ files", required=True,
+    #                         type=lambda x:
+    #                         PathFinder.check_dir_exists_and_is_nonempty(x, "Verify the '--fastqdir' argument"))
+    #     parser.add_argument('--fastadir', dest='fastadir', nargs=1, help="Directory with FASTA files", required=True,
+    #                         type=os.path.abspath)
+    #     return parser
 
 
     @staticmethod
