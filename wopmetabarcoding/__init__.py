0bdbde08a8eb262d65150f16bd3c6d668ace0792
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-09-23 21:31:47 +0200

    ENH argparser
    Former-commit-id: ef1c4e39e626559548ce591bfc91e3bbe181eb96

--- wopmetabarcoding/__init__.py
+++ wopmetabarcoding/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
-import argparse
-import sys
-
 from wopmetabarcoding.utils.ArgParser import ArgParser
+from wopmetabarcoding.utils.OptionManager import OptionManager
+
 from wopmetabarcoding.utils.OptionManager import OptionManager
 
 
@@ -11,45 +10,16 @@ class VTAM(object):
 
     def __init__(self, sys_argv):
         self.sys_argv = sys_argv
-        parser = ArgParser.get_arg_parser_base()
-        args = parser.parse_args(sys_argv[1:2])
-        # use dispatch pattern to invoke method with same name
-        getattr(self, args.command)()
-
-    def merge(self):
-        # now that we're inside a subcommand, ignore the first
-        # TWO argvs, ie the command (vtam) and the subcommand (merge)
-        parser = ArgParser.get_arg_parser_merge()
-        args = parser.parse_args(self.sys_argv[1:])
+        parser = ArgParser.get_arg_parser()
+        args = parser.parse_args(sys_argv)
         ##################################
         #
         # Store arguments in OptionsManager
         #
         ##################################
         for k in vars(args):
-            if not k is 'command':
-                try:
-                    OptionManager.instance()[k] = vars(args)[k][0]
-                except TypeError:
-                    OptionManager.instance()[k] = vars(args)[k]
+            OptionManager.instance()[k] = vars(args)[k]
 
-    def otu(self):
-        parser = argparse.ArgumentParser(
-            description='Download objects and refs from another repository')
-        # NOT prefixing the argument with -- means it's not optional
-        parser.add_argument('repository')
-        args = parser.parse_args(sys.argv[2:])
-        ##################################
-        #
-        # Store in OptionsManager
-        #
-        ##################################
-        for k in vars(args):
-            if not k is 'command':
-                try:
-                    OptionManager.instance()[k] = vars(args)[k][0]
-                except TypeError:
-                    OptionManager.instance()[k] = vars(args)[k]
 
 if __name__ == '__main__':
     VTAM()
