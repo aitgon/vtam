99a116f473264ed0056b97f7f94e795ee235b781
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-09-27 09:38:54 +0200

    ENH WopmarsRunner
    Former-commit-id: 9fae436b63a59ab3332388f998c9093202708cea

---
+++ wopmetabarcoding/utils/WopmarsRunner.py
@@ -0,0 +1,91 @@
+import jinja2
+import os
+
+import sys
+
+from wopmetabarcoding.utils.Singleton import Singleton
+from wopmetabarcoding.utils.constants import parameters_numerical
+from wopmetabarcoding.utils.utilities import tempdir
+
+
+class WopmarsRunner(Singleton):
+
+    def __init__(self, tool, parameters):
+        """
+
+        :param tool: takes one of three values: merge, otu or optimize
+        :param parameters: dictionnary (OptionManager.instance()) with command
+        """
+        self.tool = tool
+        ##################################
+        #
+        # Load default numerical parameters and overwrite with custom parameters
+        #
+        ##################################
+        self.parameters = parameters_numerical.copy()
+        for k in parameters:
+            self.parameters[k] = parameters[k]
+        #
+        self.wopfile_path = None
+
+
+    def get_wopfile(self, wopfile_path=None):
+        """
+
+        :param wopfile_path: Path of output wopfile
+        :return: string with either wopfile content (wopfile_path=None) or path to the wopfile
+        """
+        wopfile_file_name = 'Wopfile_{}.yml'.format(self.tool)
+        wopfile_in_path = os.path.join(os.path.dirname(__file__), '../../data', wopfile_file_name)
+        with open(wopfile_in_path) as fin:
+            template = jinja2.Template(fin.read())
+        wopfile_rendered = template.render(self.parameters)
+        ################
+        #
+        # return
+        #
+        ################
+        if wopfile_path is None:
+            return wopfile_rendered
+        with open(wopfile_path, "w") as fout:
+            fout.write(wopfile_rendered)
+        return wopfile_path
+
+
+    def get_wopmars_command(self):
+        """
+
+        :param wopfile_out_path: Path of output wopfile
+        :return: string with output path of wopfile
+        """
+
+        ###################
+        #
+        # Base wopmars command
+        #
+        ###################
+        if self.wopfile_path is None:
+            self.wopfile_path = os.path.join(tempdir, 'Wopfile_{}.yml'.format(self.tool))
+            self.wopfile_path = self.get_wopfile(wopfile_path=self.wopfile_path)
+        wopmars_command_template = "wopmars -w {wopfile_path} -D sqlite:///{db} -p -v"
+        wopmars_command = wopmars_command_template.format(wopfile_path=self.wopfile_path, **self.parameters)
+        # wopmars_command_template = "wopmars -w {wopfile_path} -D sqlite:///{db} -p -v".format(**self.parameters)
+        if self.parameters['dryrun']:
+            wopmars_command += " -n"
+        if self.parameters['forceall']:
+            wopmars_command += " -F"
+        if not self.parameters['params'] is None:
+            wopmars_command += " --params {params}".format(**self.parameters)
+        if not self.parameters['targetrule'] is None:
+            wopmars_command += " --targetrule {targetrule}".format(**self.parameters)
+        ###################
+        #
+        # Wopmars command options of merge, otu and optimize
+        #
+        ###################
+        if self.tool == 'merge':
+            wopmars_command += " --fastainfo {fastainfo} --fastadir {fastqdir}"
+        else:
+            sys.exit(1)
+        wopmars_command = wopmars_command.format(**self.parameters)
+        return wopmars_command
