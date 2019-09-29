f65743e2bea835d07a07d26daeee3e2da61be99a
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-09-29 09:30:44 +0200

    ENH
    Former-commit-id: c24eb2bbf377c42ee7fd0290aff30840f2ef4af8

--- wopmetabarcoding/wrapper/Merge.py
+++ wopmetabarcoding/wrapper/Merge.py
@@ -1,11 +1,15 @@
+import sys
+
 from wopmars.framework.database.tables.ToolWrapper import ToolWrapper
 import os
 
 import subprocess
-import errno
+import pathlib
 
+from wopmetabarcoding import VTAMexception
 from wopmetabarcoding.utils.Logger import Logger
 from wopmetabarcoding.utils.OptionManager import OptionManager
+from wopmetabarcoding.utils.PathManager import PathManager
 
 
 class Merge(ToolWrapper):
@@ -13,18 +17,18 @@ class Merge(ToolWrapper):
         "polymorphic_identity": "wopmetabarcoding.wrapper.Merge"
     }
 
-    __input_sample2fastq = "sample2fastq"
-    __output_sample2fasta = "sample2fasta"
+    __input_fastqinfo = "fastqinfo"
+    __output_fastainfo = "fastainfo"
 
     def specify_input_file(self):
-        return[Merge.__input_sample2fastq]
+        return[Merge.__input_fastqinfo]
 
     def specify_output_file(self):
-        return[Merge.__output_sample2fasta]
+        return[Merge.__output_fastainfo]
 
     def specify_params(self):
         return{
-            "fastq_directory": "str",
+            "fastq_dir": "str",
             "fasta_dir": "str",
             "fastq_minovlen": "int",
             "fastq_maxmergelen": "int",
@@ -51,58 +55,80 @@ class Merge(ToolWrapper):
         #
         ##########################################################
         #
-        # Options
-        # import pdb; pdb.set_trace()
-        # from wopmetabarcoding.utils.Logger import Logger
         # input files
-        sample2fastq = self.input_file(Merge.__input_sample2fastq)
+        fastqinfo = self.input_file(Merge.__input_fastqinfo)
+        #
         # output files
-        sample2fasta = self.output_file(Merge.__output_sample2fasta)
-        try:
-            # Opening the file to get all the lines stocked in the list csv_content
-            with open(sample2fastq, 'r') as csv_file:
-                next(csv_file)
-                csv_content = csv_file.readlines()
-            with open(sample2fasta, 'w') as csv_fout:
-                for line in csv_content:
+        fastainfo = self.output_file(Merge.__output_fastainfo)
+        #
+        # Options
+        fastq_dir = self.option("fastq_dir")
+        fasta_dir = self.option("fasta_dir")
+        fastq_minovlen = self.option("fastq_minovlen")
+        fastq_maxmergelen = self.option("fastq_maxmergelen")
+        fastq_minmergelen = self.option("fastq_minmergelen")
+        fastq_minlen = self.option("fastq_minlen")
+        fastq_maxee = self.option("fastq_maxee")
+        fastq_truncqual = self.option("fastq_truncqual")
+        fastq_maxns = self.option("fastq_maxns")
+        fastq_ascii = self.option("fastq_ascii")
+        threads = self.option("threads")
+        #
+        # Go
+        # Opening the file to get all the lines stocked in the list csv_content
+        fastq_and_fasta_list = [] # unique pairs of fastq files with the corresponding fasta file
+        with open(fastqinfo, 'r') as csv_file:
+            with open(fastainfo, 'w') as fastainfo_fout:
+                next(csv_file) # skip header of fastqinfo
+                fastainfo_header = "TagPair Forward	Primer Forward	TagPair Reverse	Primer Reverse	Marker name	 " \
+                                   "Biosample	Replicate	Run	Fastq_fw	Fastq_rv\n"
+                fastainfo_fout.write(fastainfo_header) # write header of fastainfo
+                for line in csv_file:
                     sample_info = line.strip().split("\t")
-                    fin_fw = os.path.join(self.option("fastq_directory"), sample_info[8])
-                    fin_rev = os.path.join(self.option("fastq_directory"), sample_info[9])
-                    fout_name = sample_info[7] + "_" + sample_info[4] + "_" +sample_info[6] + ".fasta"
+                    fastq_fw_abspath = os.path.join(fastq_dir, sample_info[8])
+                    fastq_rv_abspath = os.path.join(fastq_dir, sample_info[9])
                     try:
-                        os.makedirs(self.option("fasta_dir"))
-                    except OSError as exception:
-                        if exception.errno != errno.EEXIST:
-                            raise
-                    fout = os.path.join(self.option("fasta_dir"), fout_name)
-                    line2write = line.strip() + '\t' + fout_name + "\n"
-                    csv_fout.write(line2write)
-                    if not os.path.isfile(fout):
-                        if os.path.isfile(fin_fw) is False:
-                            error_file = sample_info[8]
-                            raise FileNotFoundError('One of the input file: ' + error_file + ' is missing.')
+                        pathlib.Path(fastq_fw_abspath).resolve(strict=True)
+                    except FileNotFoundError:
+                        Logger.instance().error(VTAMexception("VTAMexception: This FASTQ file was not found: {}.".format(fastq_fw_abspath)))
+                        sys.exit(1)
+                    try:
+                        pathlib.Path(fastq_rv_abspath).resolve(strict=True)
+                    except FileNotFoundError:
+                        Logger.instance().error(VTAMexception("VTAMexception: This FASTQ file was not found: {}.".format(fastq_rv_abspath)))
+                        sys.exit(1)
+                    fout_name = sample_info[7] + "_" + sample_info[4] + "_" +sample_info[6] + ".fasta"
+                    PathManager.mkdir_p(fasta_dir)
+                    fastainfo_line = '\t'.join(line.strip()) + '\n'
+                    fastainfo_fout.write(fastainfo_line) # write fastainfo line
+                    fasta_abspath = os.path.join(fasta_dir, fout_name)
+                    if not (fastq_fw_abspath, fastq_rv_abspath, fasta_abspath) in fastq_and_fasta_list:
+                        fastq_and_fasta_list.append((fastq_fw_abspath, fastq_rv_abspath, fasta_abspath))
 
-                        elif os.path.isfile(fin_rev) is False:
-                            error_file = sample_info[9]
-                            raise FileNotFoundError('One of the input file: ' + error_file + ' is missing.')
-                        else:
-                            command = "vsearch" + " -fastq_mergepairs " + fin_fw + " --reverse " + fin_rev
-                            command = command + " --fastaout " + fout + ' --fastq_minovlen ' + str(self.option("fastq_minovlen"))
-                            command = command + ' --fastq_maxmergelen ' + str(
-                                self.option("fastq_maxmergelen")) + ' --fastq_minmergelen ' + str(self.option("fastq_minmergelen"))
-                            command = command + " --fastq_minlen " + str(self.option("fastq_minlen")) + ' --fastq_maxee ' + str(
-                                self.option("fastq_maxee"))
-                            command = command + " --fastq_truncqual " + str(self.option("fastq_truncqual")) + " --fastq_maxns " + str(
-                                self.option("fastq_maxns"))
-                            command = command + " --threads " + str(self.option("threads")) + ' --fastq_ascii ' + str(
-                                self.option("fastq_ascii"))
-                            subprocess.call(command, shell=True)
-                    else:
-                        continue
-        except IsADirectoryError:
-            context = 'While trying to open csv file, the path is pointing to a directory and not a file'
-            raise IsADirectoryError(context + ' Please check the path you enter for the csv option')
-        except FileNotFoundError:
-            context = \
-                'While searching for the csv file or the fastq directory, any file or directory are found at the pointed directory'
-            raise FileNotFoundError(context + ' Please check if the csv file or the fastq directory are present')
+        #########################################
+        #
+        # Loop of fastq pairs and run vsearch
+        #
+        #########################################
+        for fastq_fw_abspath, fastq_rv_abspath, fasta_abspath in fastq_and_fasta_list:
+            command = "vsearch -fastq_mergepairs {} --reverse {}".format(fastq_fw_abspath, fastq_rv_abspath)
+            command += " --fastaout {} --fastq_minovlen {}".format(fasta_abspath, fastq_minovlen)
+            command += " --fastq_maxmergelen {}".format(fastq_maxmergelen)
+            command += " --fastq_minmergelen {}".format(fastq_minmergelen)
+            command += " --fastq_minlen {}".format(fastq_minlen)
+            command += " --fastq_maxee {}".format(fastq_maxee)
+            command += " --fastq_truncqual {}".format(fastq_truncqual)
+            command += " --fastq_maxns {}".format(fastq_maxns)
+            command += " --fastq_ascii {}".format(fastq_ascii)
+            command += " --threads {}".format(threads)
+            Logger.instance().info(command)
+            run_result = subprocess.run(command.split(), stdout=subprocess.PIPE)
+            Logger.instance().info(run_result.stdout)
+            Logger.instance().error(run_result.stderr)
+            try:
+                assert run_result.returncode == 0 # vsearch exited correctly?
+            except Exception:
+                msg = "Vsearch error: The program has exited due to an error." \
+                      "Verify the command and the input and output files."
+                Logger.instance().error(VTAMexception(msg))
+                sys.exit(1)
