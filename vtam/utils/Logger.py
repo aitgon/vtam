914996d5ce871e84bcc3151b5380e708e31603bf
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2019-10-25 13:28:16 +0200

    BUG
    Former-commit-id: 0d9b760a11aca39b7356d0b6a9ac64ade8587f43

--- vtam/utils/Logger.py
+++ vtam/utils/Logger.py
@@ -33,11 +33,13 @@ class Logger(Singleton):
         # log file in append mode of size 1 Mo and 1 backup
         # handler equivalent to stream_handler in term of logging level but write to .log file
         try:
-            if not OptionManager.instance()["log_file"] is None:
-                log_file_path = OptionManager.instance()["log_file"]
-                self.__file_handler = RotatingFileHandler(log_file_path, 'a', 1000000, 1)
-                self.__file_handler.setFormatter(self.__formatter)
-                self.__logger.addHandler(self.__file_handler)
+            assert not (OptionManager.instance()["log_file"] is None or OptionManager.instance()["log_file"] == 'None')
+            log_file_path = OptionManager.instance()["log_file"]
+            self.__file_handler = RotatingFileHandler(log_file_path, 'a', 1000000, 1)
+            self.__file_handler.setFormatter(self.__formatter)
+            self.__logger.addHandler(self.__file_handler)
+        except AssertionError: # ignore
+            pass
         except KeyError: # ignore
             pass
 
