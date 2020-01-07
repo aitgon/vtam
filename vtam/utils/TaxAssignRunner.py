31eee5de7c7d0cbd8a2533d5fa00e60fb3cff1f7
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-01-07 21:33:05 +0100

    ONGOING

--- vtam/utils/TaxAssignRunner.py
+++ vtam/utils/TaxAssignRunner.py
@@ -14,17 +14,18 @@ from vtam.utils.constants import rank_hierarchy,identity_list
 class TaxAssignRunner(object):
     """Will assign variants to a taxon"""
 
-    def __init__(self, variant_df, taxonomy_tsv, blast_db_dir, ltg_rule_threshold, include_prop, min_number_of_taxa, num_threads):
+    def __init__(self, variant_df, taxonomy_df, blast_db_dir, ltg_rule_threshold, include_prop, min_number_of_taxa,
+                 num_threads):
 
         self.variant_df = variant_df
-        self.taxonomy_tsv = taxonomy_tsv
+        self.taxonomy_df = taxonomy_df
         self.blast_db_dir = blast_db_dir
         self.this_temp_dir = os.path.join(PathManager.instance().get_tempdir(), os.path.basename(__file__))
         pathlib.Path(self.this_temp_dir).mkdir(exist_ok=True)
         self.ltg_rule_threshold = ltg_rule_threshold
         self.include_prop = include_prop
         self.min_number_of_taxa = min_number_of_taxa
-        self.taxonomy_db_df = self.f01_taxonomy_tsv_to_df()
+        self.num_threads = num_threads
 
         ##########################################################
         #
@@ -48,21 +49,16 @@ class TaxAssignRunner(object):
             "file: {}; line: {}; Running local blast with FASTA input {}".format(__file__,
                                                                                  inspect.currentframe().f_lineno,
                                                                                  variant_fasta))
-        #
+
         # Run and read local blast result
         blast_output_tsv = os.path.join(self.this_temp_dir, 'blast_output.tsv')
         # blast_output_tsv = "/home/gonzalez/tmp/blast/blast_output.tsv" # uncomment for testing
         # get blast db dir and filename prefix from NHR file
         os.environ['BLASTDB'] = self.blast_db_dir
-        # if os.path.basename(map_taxids_tsv_path) == 'None': # run blast with full NCBI blast db
-        #     blastn_cline = NcbiblastnCommandline(query=variant_fasta, db=blast_db_basename, evalue=1e-5,
-        #                                          outfmt='"6 qseqid sacc pident evalue qcovhsp staxids"', dust='yes',
-        #                                          qcov_hsp_perc=80, num_threads=1, out=blast_output_tsv)
-        # else: # run blast with custom blast db
-        # map_taxids_tsv_path, coi_blast_db_dir = download_coi_db()
+
         blastn_cline = NcbiblastnCommandline(query=variant_fasta, db='nt', evalue=1e-5,
                                              outfmt='"6 qseqid sacc pident evalue qcovhsp staxids"', dust='yes',
-                                             qcov_hsp_perc=80, num_threads=num_threads, out=blast_output_tsv)
+                                             qcov_hsp_perc=80, num_threads=self.num_threads, out=blast_output_tsv)
         Logger.instance().debug(
             "file: {}; line: {}; {}".format(__file__, inspect.currentframe().f_lineno, str(blastn_cline)))
         #
@@ -123,7 +119,7 @@ class TaxAssignRunner(object):
                 __file__, inspect.currentframe().f_lineno))
         lineage_list = []
         for target_tax_id in blast_output_df.target_tax_id.unique().tolist():
-            lineage_list.append(self.f04_1_tax_id_to_taxonomy_lineage(target_tax_id, self.taxonomy_db_df))
+            lineage_list.append(self.f04_1_tax_id_to_taxonomy_lineage(tax_id=target_tax_id))
         tax_id_to_lineage_df = pandas.DataFrame(lineage_list)
         #
         Logger.instance().debug(
@@ -152,32 +148,20 @@ class TaxAssignRunner(object):
         #
         self.ltg_df = self.f07_blast_result_to_ltg_tax_id(variantid_identity_lineage_df)
 
-    def f01_taxonomy_tsv_to_df(self):
+    def f04_1_tax_id_to_taxonomy_lineage(self, tax_id):
         """
-        Imports taxonomy_db.tsv file with taxonomy table (tax_id, parent_tax_id, rank, name_txt, old_tax_id)
-        into DataFrame
+        Takes tax_id and taxonomy_df and creates a dictionary with the taxonomy lineage
 
-        Args:
-            taxonomy_tsv (String): Path to taxonomy_db.tsv
+        :param tax_id: Identifier of taxon
+        :type tax_id: int
+        :param return_tax_name: (pandas.DataFrame: DataFrame with taxonomy information
+        :type return_tax_name: str
 
-
-        Returns:
-            pandas.DataFrame: with taxonomy information
-
-        """
-        taxonomy_db_df = pandas.read_csv(self.taxonomy_tsv, sep="\t", header=0,
-                                         dtype={'tax_id': 'int', 'parent_tax_id': 'int', 'old_tax_id': 'float'})
-        return taxonomy_db_df
-
-
-    def f04_1_tax_id_to_taxonomy_lineage(self, tax_id, return_tax_name=False):
-        """
-        Takes tax_id and taxonomy_db.tsv DB and create a dictionary with the taxonomy lineage
-
+        :return: Dic with ranks/keys and tax_id/values
+        :rtype: list
         Args:
             tax_id (Integer): Identifier of taxon
-            taxonomy_db_df (pandas.DataFrame: DataFrame with taxonomy information
-
+            taxonomy_df (pandas.DataFrame: DataFrame with taxonomy information
 
         Returns:
             Dictionnary: with taxonomy information for given tax_id, {'tax_id': 183142, 'species': 183142, 'genus': 10194, 'family': 10193, 'order': 84394,
@@ -185,22 +169,48 @@ class TaxAssignRunner(object):
                              'superkingdom': 2759}
 
         """
-        lineage_dic = {}
-        lineage_dic['tax_id'] = tax_id
+
+        lineage_dic = {'tax_id': tax_id}
         while tax_id != 1:
-            # try to use taxonomy_db_df.tax_id
-            tax_id_row = self.taxonomy_db_df.loc[self.taxonomy_db_df.tax_id == tax_id,]
+            # try to use taxonomy_df.tax_id
+            tax_id_row = self.taxonomy_df.loc[self.taxonomy_df.tax_id == tax_id,]
             # row empty, try to use old_tax_id
             if tax_id_row.shape[0] == 0:
-                tax_id_row = self.taxonomy_db_df.loc[self.taxonomy_db_df.old_tax_id == tax_id,]
+                tax_id_row = self.taxonomy_df.loc[self.taxonomy_df.old_tax_id == tax_id,]
             rank = tax_id_row['rank'].values[0]
             parent_tax_id = tax_id_row['parent_tax_id'].values[0]
             lineage_dic[rank] = tax_id
-            if return_tax_name: # return tax_name instead of tax_id
-                tax_name = tax_id_row['name_txt'].values[0]
-                lineage_dic[rank] = tax_name
+            # if return_tax_name: # return tax_name instead of tax_id
+            #     tax_name = tax_id_row['name_txt'].values[0]
+            #     lineage_dic[rank] = tax_name
             tax_id = parent_tax_id
         return lineage_dic
+
+    def f06_select_ltg(self, tax_lineage_df):
+        """
+        Given tax_lineage_df, selects the LTG
+
+        Args:
+            tax_lineage_df (pandas.DataFrame): DF where each column is a rank, rows are different target_ids and values are putative_ltg_ids
+            include_prop (int): Percentage out of total selected qblast hits for Ltg to be present when identity>=ltg_rule_threshold
+
+        Returns:
+            ltg_tax_id (int): Taxonomical ID of Ltg
+            ltg_rank (str): Rank of Ltg
+
+        """
+
+        # Remove column if all values=NaN
+        tax_lineage_df.dropna(axis='columns', how='all', inplace=True)
+        lineage_list_df_columns_sorted = list(filter(lambda x: x in tax_lineage_df.columns.tolist(), rank_hierarchy))
+        tax_lineage_df = tax_lineage_df[lineage_list_df_columns_sorted]
+        putative_ltg_df = \
+            pandas.DataFrame({'putative_ltg_id': tax_lineage_df.apply(lambda x: x.value_counts().index[0], axis=0),
+                              'putative_ltg_count': tax_lineage_df.apply(lambda x: x.value_counts().iloc[0], axis=0)})
+        putative_ltg_df['putative_ltg_percentage'] = putative_ltg_df.putative_ltg_count / tax_lineage_df.shape[0] * 100
+        ltg_tax_id = putative_ltg_df.loc[putative_ltg_df.putative_ltg_percentage >= self.include_prop, 'putative_ltg_id'].tail(1).values[0]
+        ltg_rank = putative_ltg_df.loc[putative_ltg_df.putative_ltg_percentage >= self.include_prop, 'putative_ltg_id'].index[-1]
+        return ltg_tax_id, ltg_rank
 
 
     def f07_blast_result_to_ltg_tax_id(self, variantid_identity_lineage_df):
@@ -263,7 +273,7 @@ class TaxAssignRunner(object):
                         # drop column with NaN
                         tax_lineage_by_variant_id_df = tax_lineage_by_variant_id_df.dropna(axis='columns', how='all')
                         #
-                        ltg_tax_id, ltg_rank = f06_select_ltg(tax_lineage_by_variant_id_df, include_prop=self.include_prop)
+                        ltg_tax_id, ltg_rank = self.f06_select_ltg(tax_lineage_by_variant_id_df)
                         # ltg_tax_id, ltg_rank = None, None
                         # if identity >= ltg_rule_threshold:
                         #     #
@@ -378,24 +388,6 @@ def f07_blast_result_to_ltg_tax_id(variantid_identity_lineage_df, ltg_rule_thres
     return ltg_df
 
 
-def f01_taxonomy_tsv_to_df(taxonomy_tsv):
-    """
-    Imports taxonomy_db.tsv file with taxonomy table (tax_id, parent_tax_id, rank, name_txt, old_tax_id)
-    into DataFrame
-
-    Args:
-        taxonomy_tsv (String): Path to taxonomy_db.tsv
-
-
-    Returns:
-        pandas.DataFrame: with taxonomy information
-
-    """
-    taxonomy_db_df = pandas.read_csv(taxonomy_tsv, sep="\t", header=0,
-                                     dtype={'tax_id': 'int', 'parent_tax_id': 'int', 'old_tax_id': 'float'})
-    return taxonomy_db_df
-
-
 def f04_1_tax_id_to_taxonomy_lineage(tax_id, taxonomy_db_df, give_tax_name=False):
     """
     Takes tax_id and taxonomy_db.tsv DB and create a dictionary with the taxonomy lineage
@@ -414,7 +406,7 @@ def f04_1_tax_id_to_taxonomy_lineage(tax_id, taxonomy_db_df, give_tax_name=False
     lineage_dic = {}
     lineage_dic['tax_id'] = tax_id
     while tax_id != 1:
-        # try to use taxonomy_db_df.tax_id
+        # try to use taxonomy_df.tax_id
         tax_id_row = taxonomy_db_df.loc[taxonomy_db_df.tax_id == tax_id,]
         # row empty, try to use old_tax_id
         if tax_id_row.shape[0] == 0:
@@ -428,33 +420,34 @@ def f04_1_tax_id_to_taxonomy_lineage(tax_id, taxonomy_db_df, give_tax_name=False
         tax_id = parent_tax_id
     return lineage_dic
 
-def f05_blast_result_subset(qblast_result_subset_df, taxonomy_db_df):
-    """
-    Takes qblast_result_subset_df and returns tax_lineage_df with lineages of each target_tax_id
 
-    Args:
-        qblast_result_subset_df (DataFrame): DataFrame with result subset for given identity
-            where each column is target_id and target_tax_id
-    target_id  target_tax_id
-0  1049499563         761875
-1  1049496963         761875
-        taxonomy_db_df (pandas.DataFrame: DataFrame with taxonomy information
-
-    Returns:
-        DataFrame: with a lineage per row that corresponds to the the lineage of each taxon from the qblast sequence targets
-   class  cohort  family  genus  infraclass  kingdom  no rank  order  phylum  species  subclass  subfamily  suborder  subphylum  superfamily  superkingdom  superorder
-0  50557   33392   41030  50443       33340    33208   131567  30263    6656   761875      7496     147297     93873       6960        41029          2759       85604
-1  50557   33392   41030  50443       33340    33208   131567  30263    6656   761875      7496     147297     93873       6960        41029          2759       85604
-
-    """
-    lineage_list = []
-    for target_tax_id in qblast_result_subset_df.target_tax_id.unique().tolist():
-        lineage_list.append(f04_1_tax_id_to_taxonomy_lineage(target_tax_id, taxonomy_db_df))
-    tax_lineage_df = pandas.DataFrame(lineage_list)
-    tax_lineage_df = qblast_result_subset_df.merge(tax_lineage_df, left_on='target_tax_id', right_on='tax_id')
-    tax_lineage_df.drop('target_id', axis=1, inplace=True)
-    tax_lineage_df.drop('target_tax_id', axis=1, inplace=True)
-    return tax_lineage_df
+# def f05_blast_result_subset(qblast_result_subset_df, taxonomy_db_df):
+#     """
+#     Takes qblast_result_subset_df and returns tax_lineage_df with lineages of each target_tax_id
+#
+#     Args:
+#         qblast_result_subset_df (DataFrame): DataFrame with result subset for given identity
+#             where each column is target_id and target_tax_id
+#     target_id  target_tax_id
+# 0  1049499563         761875
+# 1  1049496963         761875
+#         taxonomy_df (pandas.DataFrame: DataFrame with taxonomy information
+#
+#     Returns:
+#         DataFrame: with a lineage per row that corresponds to the the lineage of each taxon from the qblast sequence targets
+#    class  cohort  family  genus  infraclass  kingdom  no rank  order  phylum  species  subclass  subfamily  suborder  subphylum  superfamily  superkingdom  superorder
+# 0  50557   33392   41030  50443       33340    33208   131567  30263    6656   761875      7496     147297     93873       6960        41029          2759       85604
+# 1  50557   33392   41030  50443       33340    33208   131567  30263    6656   761875      7496     147297     93873       6960        41029          2759       85604
+#
+#     """
+#     lineage_list = []
+#     for target_tax_id in qblast_result_subset_df.target_tax_id.unique().tolist():
+#         lineage_list.append(f04_1_tax_id_to_taxonomy_lineage(target_tax_id, taxonomy_db_df))
+#     tax_lineage_df = pandas.DataFrame(lineage_list)
+#     tax_lineage_df = qblast_result_subset_df.merge(tax_lineage_df, left_on='target_tax_id', right_on='tax_id')
+#     tax_lineage_df.drop('target_id', axis=1, inplace=True)
+#     tax_lineage_df.drop('target_tax_id', axis=1, inplace=True)
+#     return tax_lineage_df
 
 
 def f06_select_ltg(tax_lineage_df, include_prop):
@@ -470,11 +463,14 @@ def f06_select_ltg(tax_lineage_df, include_prop):
         ltg_rank (str): Rank of Ltg
 
     """
+
     # Remove column if all values=NaN
     tax_lineage_df.dropna(axis='columns', how='all', inplace=True)
     lineage_list_df_columns_sorted = list(filter(lambda x: x in tax_lineage_df.columns.tolist(), rank_hierarchy))
     tax_lineage_df = tax_lineage_df[lineage_list_df_columns_sorted]
-    putative_ltg_df = pandas.DataFrame({'putative_ltg_id': tax_lineage_df.apply(lambda x: x.value_counts().index[0], axis=0),'putative_ltg_count': tax_lineage_df.apply(lambda x: x.value_counts().iloc[0], axis=0)})
+    putative_ltg_df = \
+        pandas.DataFrame({'putative_ltg_id': tax_lineage_df.apply(lambda x: x.value_counts().index[0], axis=0),
+                          'putative_ltg_count': tax_lineage_df.apply(lambda x: x.value_counts().iloc[0], axis=0)})
     putative_ltg_df['putative_ltg_percentage'] = putative_ltg_df.putative_ltg_count / tax_lineage_df.shape[0] * 100
     ltg_tax_id = putative_ltg_df.loc[putative_ltg_df.putative_ltg_percentage >= include_prop, 'putative_ltg_id'].tail(1).values[0]
     ltg_rank = putative_ltg_df.loc[putative_ltg_df.putative_ltg_percentage >= include_prop, 'putative_ltg_id'].index[-1]
