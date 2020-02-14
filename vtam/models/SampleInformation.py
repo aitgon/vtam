002213342b38558688cbd1fa73e24f0220fbaa3d
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-02-14 21:10:48 +0100

    ENH updated sample information
    Former-commit-id: c198a229cb5e740214adc44735722ee0b0a28bfc

--- vtam/models/SampleInformation.py
+++ vtam/models/SampleInformation.py
@@ -17,10 +17,10 @@ class SampleInformation(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     run_id = Column(Integer, ForeignKey("Run.id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
     marker_id = Column(Integer, ForeignKey("Marker.id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
-    primer_forward = Column(String(100), nullable=False)
-    primer_reverse = Column(String(100), nullable=False)
-    tag_forward = Column(String(100), nullable=False)
-    tag_reverse = Column(String(100), nullable=False)
+    # primer_forward = Column(String(100), nullable=False)
+    # primer_reverse = Column(String(100), nullable=False)
+    # tag_forward = Column(String(100), nullable=False)
+    # tag_reverse = Column(String(100), nullable=False)
     fasta_id = Column(Integer, ForeignKey("Fasta.id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
     biosample_id = Column(Integer, ForeignKey("Biosample.id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
     replicate = Column(Integer, nullable=False)
@@ -34,8 +34,8 @@ class SampleInformation(Base):
             assert name.isalnum()
         return name
 
-    @validates('tag_forward', 'primer_forward', 'tag_reverse', 'primer_reverse')
-    def validate_sequences(self, key, sequence):
-        if sequence != "":
-            assert Seq(sequence, IUPAC.ambiguous_dna)
-        return sequence
+    # @validates('tag_forward', 'primer_forward', 'tag_reverse', 'primer_reverse')
+    # def validate_sequences(self, key, sequence):
+    #     if sequence != "":
+    #         assert Seq(sequence, IUPAC.ambiguous_dna)
+    #     return sequence
