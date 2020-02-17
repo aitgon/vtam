70f633c0fc3e4c9920471d9f3b466a9fc7e1c1c0
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-02-17 21:24:11 +0100

    CLN

--- vtam/wrapper/SampleInformation.py
+++ vtam/wrapper/SampleInformation.py
@@ -87,83 +87,10 @@ class SampleInformation(ToolWrapper):
             fasta_id = fasta_instance.id
 
             # Insert sample_information
-            sample_information_obj = {'biosample_id': biosample_id, 'replicate': replicate,
-                                      'run_id': run_id}
+            sample_information_obj = {'biosample_id': biosample_id, 'replicate': replicate, 'run_id': run_id}
             sample_information_obj['fasta_id'] = fasta_id
             sample_information_obj['marker_id'] = marker_id
             SampleInformation.get_or_create(session, sampleinformation_model, **sample_information_obj)
-
-            # # Primer pair
-            # primerpair_obj = {'primer_forward': primer_forward, 'primer_reverse': primer_reverse}
-            # SampleInformation.get_or_create(session, primerpair_model, **primerpair_obj)
-            #
-            # # Tag pair
-            # tagpair_obj = {'tag_forward': tag_forward, 'tag_reverse': tag_reverse}
-            # SampleInformation.get_or_create(session, tagpair_model, **tagpair_obj)
-
-
-        # with open(csv_path, 'r') as fin:
-        #     next(fin) # skip header
-        #     for line in fin:
-        #         tag_forward = line.strip().split('\t')[0]
-        #         primer_forward = line.strip().split('\t')[1]
-        #         tag_reverse = line.strip().split('\t')[2]
-        #         primer_reverse = line.strip().split('\t')[3]
-        #         marker_name = line.strip().split('\t')[4]
-        #         biosample_name = line.strip().split('\t')[5]
-        #         # replicate_name = line.strip().split('\t')[6]
-        #         replicate = line.strip().split('\t')[6]
-        #         if len(line.strip().split('\t')) == 9: # No fastq file columns
-        #             fasta_file_name = line.strip().split('\t')[8]
-        #             # fasta_file_full_path = os.path.join(self.option("fasta_dir"), line.strip().split('\t')[8])
-        #             fasta_file_full_path = os.path.join(fasta_dir, line.strip().split('\t')[8])
-        #         elif len(line.strip().split('\t')) == 11: # Yes fastq file columns
-        #             fasta_file_name = line.strip().split('\t')[10]
-        #
-        #             # fasta_file_full_path = os.path.join(self.option("fasta_dir"), line.strip().split('\t')[10])
-        #             fasta_file_full_path = os.path.join(fasta_dir, line.strip().split('\t')[10])
-        #         else:
-        #             raise IndexError("{} error. Verify nb of columns in this input file: {}".format(self.__class__.__name__, os.path.join(os.getcwd(), csv_path)))
-        #         if not os.path.isfile(os.path.join(os.getcwd(), fasta_file_full_path)):
-        #             raise FileNotFoundError("{} error. Verify this file output: {}".format(self.__class__.__name__, os.path.join(os.getcwd(), fasta_file_full_path)))
-        #         run_name = line.strip().split('\t')[7]
-        #         #
-        #         # Insert run
-        #         run_obj = {'name': run_name}
-        #         run_instance = SampleInformation.get_or_create(session, run_model, **run_obj)
-        #         run_id = run_instance.id
-        #         #
-        #         # Insert marker_id
-        #         marker_obj = {'name': marker_name}
-        #         marker_instance = SampleInformation.get_or_create(session, marker_model, **marker_obj)
-        #         marker_id = marker_instance.id
-        #         #
-        #         # Insert Biosamples
-        #         biosample_obj = {'name': biosample_name}
-        #         biosample_instance = SampleInformation.get_or_create(session, biosample_model, **biosample_obj)
-        #         biosample_id = biosample_instance.id
-        #         #
-        #         # Insert file output
-        #         is_trimmed = False # Default
-        #         fasta_obj = {'name': fasta_file_name, 'is_trimmed': is_trimmed, 'run_id': run_id}
-        #         fasta_instance = SampleInformation.get_or_create(session, fasta_model, **fasta_obj)
-        #         fasta_id = fasta_instance.id
-        #
-        #         # Insert sample_information
-        #         sample_information_obj = {'tag_forward': tag_forward, 'primer_forward': primer_forward, 'tag_reverse':
-        #             tag_reverse, 'primer_reverse': primer_reverse, 'biosample_id': biosample_id, 'replicate': replicate,
-        #                                   'run_id': run_id}
-        #         sample_information_obj['fasta_id'] = fasta_id
-        #         sample_information_obj['marker_id'] = marker_id
-        #         SampleInformation.get_or_create(session, sampleinformation_model, **sample_information_obj)
-        #
-        #         # Primer pair
-        #         primerpair_obj = {'primer_forward': primer_forward, 'primer_reverse': primer_reverse}
-        #         SampleInformation.get_or_create(session, primerpair_model, **primerpair_obj)
-        #
-        #         # Tag pair
-        #         tagpair_obj = {'tag_forward': tag_forward, 'tag_reverse': tag_reverse}
-        #         SampleInformation.get_or_create(session, tagpair_model, **tagpair_obj)
 
     @staticmethod
     def get_or_create(session, model, **kwargs):
