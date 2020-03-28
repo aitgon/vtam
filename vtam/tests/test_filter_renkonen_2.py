b26f15c8577c4b03c8b82165b1564e61adba7771
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-03-28 11:49:52 +0100

    TST renkonen filter
    Former-commit-id: 123b1ee2a2293de3ec459c0903902b7289d1d9cd

--- vtam/tests/test_filter_renkonen_2.py
+++ vtam/tests/test_filter_renkonen_2.py
@@ -3,61 +3,75 @@ import os
 from unittest import TestCase
 
 import pandas
-import io
 
+from vtam.utils.FilterRenkonenRunner import FilterRenkonenRunner
 from vtam.utils.PathManager import PathManager
 
 
 class TestFilterRenkonen2(TestCase):
 
-    def setUp(self):
-        pass
+    @classmethod
+    def setUpClass(cls):
+        test_filter_renkonen_input_tsv_path = os.path.join(PathManager.get_test_path(), 'test_files/test_filter_renkonen_input.tsv')
+        cls.test_filter_renkonen_distance_tsv_path = os.path.join(PathManager.get_test_path(), 'test_files/test_filter_renkonen_distance.tsv')
+        cls.upper_renkonen_tail = 0.1
+
+        cls.variant_read_count_df = pandas.read_csv(test_filter_renkonen_input_tsv_path, sep="\t", header=0)
+        cls.filter_renkonen_runner_obj = FilterRenkonenRunner(cls.variant_read_count_df)
+        cls.renkonen_distance_df = cls.filter_renkonen_runner_obj.get_renkonen_distance_df()
+
+    def test_renkonen_distance(self):
+
+        run = 1
+        marker = 1
+        biosample = '14Ben01'
+        run_marker_biosample_df = self.variant_read_count_df.loc[
+            (self.variant_read_count_df.run_id == run) & (self.variant_read_count_df.marker_id == marker) & (
+                    self.variant_read_count_df.biosample_id == biosample)]
+        renkonen_distance = self.filter_renkonen_runner_obj.get_renkonen_distance(run_marker_biosample_df,
+                                                              replicate_left='14Ben01-R1', replicate_right='14Ben01-R2')
+
+        self.assertTrue(renkonen_distance, 0.017426040192591086)
+
+    def test_renkonen_distance_df(self):
+
+        out_bak_df = pandas.read_csv(self.test_filter_renkonen_distance_tsv_path, sep="\t", header=0)
+        # import pdb; pdb.set_trace()
+        self.assertTrue(out_bak_df.to_string() == self.renkonen_distance_df.to_string())
 
     def test_1(self):
 
-        test_filter_renkonen_input_tsv_path = os.path.join(PathManager.get_test_path(), 'test_files/test_filter_renkonen_input.tsv')
-        test_filter_renkonen_distance_tsv_path = os.path.join(PathManager.get_test_path(), 'test_files/test_filter_renkonen_distance.tsv')
-        # with open(test_filter_renkonen_tsv_path, 'r') as fin:
-        df = pandas.read_csv(test_filter_renkonen_input_tsv_path, sep="\t", header=0)
+        filter_out_df = self.variant_read_count_df.copy()
+        filter_out_df['filter_delete'] = False
 
-        ################################################################################################################
-        #
-        # Start with read count df composed of 1 run, marker and biosample, 2 replicates and any number of variants
-        # Outputs a decimal between 0 and 1
-        #
-        ################################################################################################################
+        nb_of_replicates_df = self.variant_read_count_df[['run_id', 'marker_id', 'biosample_id', 'replicate']].drop_duplicates().groupby(
+            ['run_id', 'marker_id', 'biosample_id']).count().reset_index()
+        nb_of_replicates_df.rename({'replicate': 'nb_replicates'}, axis=1, inplace=True)
 
-        out_df = pandas.DataFrame()
+        self.renkonen_distance_df[
+            'above_upper_renkonen_tail'] = self.renkonen_distance_df.renkonen_distance > self.upper_renkonen_tail
+        for run_marker_biosample_replicate_row_i, run_marker_biosample_replicate_row in \
+                self.variant_read_count_df[['run_id', 'marker_id', 'biosample_id', 'replicate']].drop_duplicates().iterrows():
+            run_id = run_marker_biosample_replicate_row.run_id
+            marker_id = run_marker_biosample_replicate_row.marker_id
+            biosample_id = run_marker_biosample_replicate_row.biosample_id
+            replicate = run_marker_biosample_replicate_row.replicate
 
-        for pos, row in df[['run_id', 'marker_id', 'biosample_id']].drop_duplicates().iterrows():
+            run_marker_biosample_replicate_renkonen_df = self.renkonen_distance_df.loc[
+                (self.renkonen_distance_df.run_id == run_id) &
+                (self.renkonen_distance_df.marker_id == marker_id) &
+                (self.renkonen_distance_df.biosample_id == biosample_id) &
+                ((self.renkonen_distance_df.replicate_left == replicate) | (self.renkonen_distance_df.replicate_right == replicate))
+            ]
 
-            run_id = row.run_id
-            marker_id = row.marker_id
-            biosample_id = row.biosample_id
+            nb_replicates = int(nb_of_replicates_df.loc[
+                (nb_of_replicates_df.run_id == run_id) & (nb_of_replicates_df.marker_id == marker_id) & (
+                            nb_of_replicates_df.biosample_id == biosample_id), 'nb_replicates'].values)
 
-            biosample_df = df.loc[(df.run_id == run_id) & (df.marker_id == marker_id) & (
-                    df.biosample_id == biosample_id)]
+            delete_replicate = run_marker_biosample_replicate_renkonen_df.above_upper_renkonen_tail.sum() > (nb_replicates - 1) / 2
+            filter_out_df.loc[(filter_out_df.run_id == run_id) & (filter_out_df.marker_id == marker_id) & (
+                            filter_out_df.biosample_id == biosample_id) & (filter_out_df.replicate == replicate), 'filter_delete'] = delete_replicate
 
-            replicate_pairs = list(itertools.combinations(biosample_df.replicate.unique(), 2))
-
-            for replicate_left, replicate_right in replicate_pairs:
-
-                replicate_pair_df = biosample_df.loc[(biosample_df.replicate == replicate_left) | (
-                        biosample_df.replicate == replicate_right)]
-
-                N_j_k = replicate_pair_df.groupby(by=['run_id', 'marker_id', 'biosample_id', 'replicate']).sum().reset_index()
-                replicate_pair_df = replicate_pair_df.merge(N_j_k, left_on=['run_id', 'marker_id', 'biosample_id', 'replicate'], right_on=['run_id', 'marker_id', 'biosample_id', 'replicate'])
-                replicate_pair_df.rename({'read_count_x': 'N_i_j_k', 'read_count_y': 'N_j_k'}, axis=1, inplace=True)
-                replicate_pair_df['N_i_j_k/N_j_k'] = replicate_pair_df['N_i_j_k'] / replicate_pair_df['N_j_k']
-                replicate_pair_df = replicate_pair_df.groupby(by=['run_id', 'marker_id', 'variant_id', 'biosample_id']).min()
-                renkonen_distance = 1 - replicate_pair_df['N_i_j_k/N_j_k'].sum()
-                out_df = pandas.concat([out_df, pandas.DataFrame({'run_id': run_id, 'marker_id': marker_id,
-                                                                  'biosample_id': biosample_id,
-                                'replicate_left': replicate_left, 'replicate_right': replicate_left,
-                                'renkonen_distance': renkonen_distance}, index=[0])], axis=0)
-        out_df = out_df.reset_index(drop=True)
-
-        out_bak_df = pandas.read_csv(test_filter_renkonen_distance_tsv_path, sep="\t", header=0)
-        self.assertTrue(out_bak_df.to_string() == out_df.to_string())
-
-
+        # run_marker_biosample_row_df = self.renkonen_distance_df.merge(pandas.DataFrame(run_marker_biosample_row).T,
+        #                                                               on=['run_id', 'marker_id', 'biosample_id'])
+        import pdb; pdb.set_trace()
