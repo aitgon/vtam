19b0acd8a924322aebd807d75ad021b59d537ab0
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-04-02 16:33:17 +0200

    ENH implemented sortreads with cutadapt

--- vtam/CommandSortReads.py
+++ vtam/CommandSortReads.py
@@ -1,70 +1,17 @@
 import multiprocessing
 import os
+import shlex
+import shutil
+import subprocess
+
 import pandas
 import pathlib
-import sys
-import yaml
 
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
+
+from vtam.utils.PathManager import PathManager
 from vtam.utils.Logger import Logger
-from vtam.utils.SortReadsRunner import SortReadsRunner
-from vtam.utils.VSearch import VSearch
-from vtam.utils.VTAMexception import VTAMexception
-
-from vtam.utils.constants import parameters_numerical
-
-class VSearchMergeRunner(object):
-
-    parameters = {
-    'fastq_ascii': 33,
-    'fastq_maxee': 1,
-    'fastq_maxmergelen': 500,
-    'fastq_maxns': 0,
-    'fastq_minlen': 50,
-    'fastq_minmergelen': 100,
-    'fastq_minovlen': 50,
-    'fastq_truncqual': 10,
-    "threads": int(multiprocessing.cpu_count()),
-    }
-
-    def __init__(self, fastq_fw_abspath, fastq_rv_abspath, fasta_abspath, params_yml=None, threads=None):
-
-        self.fastq_fw_abspath = fastq_fw_abspath
-        self.fastq_rv_abspath = fastq_rv_abspath
-        self.fasta_abspath = fasta_abspath
-        self.params_yml = params_yml
-        self.threads = threads
-
-    def load_parameters(self):
-
-        self.parameters['fastq_mergepairs'] = self.fastq_fw_abspath
-        self.parameters['reverse'] = self.fastq_rv_abspath
-        self.parameters['fastaout'] = self.fasta_abspath
-        if not (self.threads is None):
-            self.parameters['threads'] = self.threads
-
-        # Read parameters
-        if not (self.params_yml is None):
-            with open(self.params_yml, 'r') as fin:
-                user_params_dic = yaml.load(fin, Loader=yaml.SafeLoader)
-                for k_in in user_params_dic:
-                    if k_in in VSearchMergeRunner.parameters.keys():
-                        self.parameters[k_in] = user_params_dic[k_in]
-                    else:
-                        Logger.instance().error(
-                            VTAMexception("One of the Merge parameters is not used by VSearch. "
-                                          "Please set one or more of these parameters: {}".format(self.parameters.keys())))
-                        sys.exit(1)
-
-    def run(self):
-
-        self.load_parameters()  # update vsearch parameters from user
-
-        # Add double dash '--' to all parameters to create vsearch parameters
-        vsearch_parameters = {}
-        for par in self.parameters:
-            vsearch_parameters['--{}'.format(par)] = self.parameters[par]
-        vsearch_cluster = VSearch(parameters=vsearch_parameters)
-        vsearch_cluster.run()
 
 
 class CommandSortReads(object):
@@ -73,56 +20,155 @@ class CommandSortReads(object):
     @classmethod
     def main(cls, fastainfo, fastadir, outdir, params=None, num_threads=multiprocessing.cpu_count()):
 
-        min_id = parameters_numerical['min_id']
-        minseqlength = parameters_numerical['minseqlength']
-        overhang = parameters_numerical['overhang']
-
-        if not (params is None):
-            min_id = params['min_id']
-            minseqlength = params['minseqlength']
-            overhang = params['overhang']
-
-        fastainfo_df = pandas.read_csv(fastainfo, sep='\t', header=0)
-
-        pathlib.Path(outdir).mkdir(exist_ok=True)
-
-        sorted_read_info_df = pandas.DataFrame()
-
         ################################################################################################################
         #
         # Loop over fasta files to sort reads per fasta
         #
         ################################################################################################################
 
-        for fasta_filename in sorted(fastainfo_df.Fasta.unique().tolist()):
+        fastainfo_df = pandas.read_csv(fastainfo, sep='\t', header=0)
+        fastainfo_df.columns = fastainfo_df.columns.str.lower()
+
+        pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
+
+        tempdir = PathManager.instance().get_tempdir()
+        sorted_read_info_df = pandas.DataFrame()
+
+        read_min_length = 150
+        read_max_length = 200
+
+        for i, fasta_filename in enumerate(sorted(fastainfo_df.fasta.unique().tolist())):
 
             Logger.instance().debug("Analysing FASTA file: {}".format(fasta_filename))
 
-            fasta_path = os.path.join(fastadir, fasta_filename)
-            fasta_info_df_i = fastainfo_df.loc[fastainfo_df.Fasta == fasta_filename]
+            fasta_info_df_i = fastainfo_df.loc[fastainfo_df.fasta == fasta_filename]
 
-            alignement_parameters = {'min_id': min_id, 'minseqlength': minseqlength, 'overhang': overhang}
+            tag_fwd = fasta_info_df_i.tagfwd[0]
+            tag_rev = fasta_info_df_i.tagrev[0]
+            primer_fwd = fasta_info_df_i.primerfwd[0]
+            primer_rev = fasta_info_df_i.primerrev[0]
+            in_fasta_basename = fasta_info_df_i.fasta[0]
+            in_raw_fasta_path = os.path.join(fastadir, in_fasta_basename)
 
-            # Create SortReadsRunner
-            sort_reads_runner = SortReadsRunner(fasta_information_df=fasta_info_df_i,
-                                                fasta_path=fasta_path,
-                                                alignement_parameters=alignement_parameters, outdir=outdir, num_threads=num_threads)
-
-            sorted_read_info_df_i, sorted_read_dic = sort_reads_runner.run()
-            sorted_read_info_df = sorted_read_info_df.append(sorted_read_info_df_i)
-
-            ################################################################################################################
+            ############################################################################################################
             #
-            # Write to trimmed fasta
+            # Cut adapt tag of forward reads
             #
-            ################################################################################################################
+            ############################################################################################################
 
-            for sorted_read_filename in sorted_read_dic:
+            tag_rev_rc = str(Seq(tag_rev, generic_dna).reverse_complement())
+            out_fasta_basename = os.path.basename(in_raw_fasta_path).replace('.fasta', '_sorted_%03d.fasta' % i)
+            out_fasta_path = os.path.join(tempdir, out_fasta_basename)
 
-                sorted_read_list = sorted_read_dic[sorted_read_filename]
-                sorted_read_path = os.path.join(outdir, sorted_read_filename)
-                with open(sorted_read_path, "w") as fout:
-                    fout.write("\n".join(sorted_read_list))
+            cmd_cutadapt_tag_dic = {'tag_fwd': tag_fwd, 'tag_fwd_len': len(tag_fwd), 'tag_rev_rc': tag_rev_rc,
+                        'tag_rev_rc_len': len(tag_rev_rc), 'num_threads': num_threads, 'in_fasta_path': in_raw_fasta_path,
+                                'out_fasta': out_fasta_path}
+            cmd_cutadapt_tag_str = "cutadapt --cores={num_threads} --no-indels -e 0 --trimmed-only " \
+                               "-g '{tag_fwd};min_overlap={tag_fwd_len}...{tag_rev_rc};min_overlap={tag_rev_rc_len}' " \
+                               "-o {out_fasta} {in_fasta_path}".format(**cmd_cutadapt_tag_dic)
+
+            Logger.instance().debug("Running: {}".format(cmd_cutadapt_tag_str))
+            run_result = subprocess.run(shlex.split(cmd_cutadapt_tag_str), stdout=subprocess.PIPE)
+            Logger.instance().info(str(run_result.stdout))
+
+            ############################################################################################################
+            #
+            # Trim primers from output
+            # (Error allowed, 0.1 by default; minimum overlap: length of the primer; no indel; multithreading)
+            #
+            ############################################################################################################
+            #   cutadapt --cores=THREADS --no-indels -e ERROR_RATE -m MINIMUM_LENGTH -M MAXIMUM_LENGTH --trimmed-only
+            #   -g "PRIMER_FW_SEQ;min_overlap=LENGTH_OF_PRIMER_FW...PRIMER_REV_REVCOMP_SEQ;min_overlap=LENGTH_OF_PRIMER_REV"
+            #   -o TRIMMED_MARKER_RUN_SAMPLE_REPLICATE.fasta DEMULT_MARKER_RUN_SAMPLE_REPLICATE.fasta
+
+            primer_rev_rc = str(Seq(primer_rev, generic_dna).reverse_complement())
+            in_fasta_path = out_fasta_path
+            out_fasta_basename = os.path.basename(in_fasta_path).replace('_sorted_%03d.fasta'%i, '_sorted_trimmed_%03d.fasta'%i)
+            out_fasta_path = os.path.join(tempdir, out_fasta_basename)
+
+            cmd_cutadapt_primer_dic = {'primer_fwd': primer_fwd, 'primer_fwd_len': len(primer_fwd), 'primer_rev_rc': primer_rev_rc,
+                        'primer_rev_rc_len': len(primer_rev_rc), 'num_threads': num_threads, 'in_fasta_path': in_fasta_path,
+                                'out_fasta': out_fasta_path, 'error_rate': 0.1, 'read_min_length': read_min_length,
+                                       'read_max_length': read_max_length}
+            cmd_cutadapt_primer_str = "cutadapt --cores={num_threads} --no-indels -e {error_rate} -m {read_min_length} " \
+                                      "-M {read_max_length} --trimmed-only  " \
+                                      "-g '{primer_fwd};min_overlap={primer_fwd_len}...{primer_rev_rc};min_overlap={primer_rev_rc_len}' " \
+                               "-o {out_fasta} {in_fasta_path}".format(**cmd_cutadapt_primer_dic)
+
+            Logger.instance().debug("Running: {}".format(cmd_cutadapt_primer_str))
+            run_result = subprocess.run(shlex.split(cmd_cutadapt_primer_str), stdout=subprocess.PIPE)
+            Logger.instance().info(str(run_result.stdout))
+
+            ############################################################################################################
+            #
+            # Cut adapt tag of reverse-complement reads
+            #
+            ############################################################################################################
+
+            tag_fwd_rc = str(Seq(tag_fwd, generic_dna).reverse_complement())
+            out_rc_fasta_basename= os.path.basename(in_raw_fasta_path).replace('.fasta', '_rc_sorted_%03d.fasta' % i)
+            out_rc_fasta_path = os.path.join(tempdir, out_rc_fasta_basename)
+
+            cmd_cutadapt_tag_dic = {'tag_fwd': tag_rev, 'tag_fwd_len': len(tag_rev), 'tag_rev_rc': tag_fwd_rc,
+                        'tag_rev_rc_len': len(tag_fwd_rc), 'num_threads': num_threads, 'in_fasta_path': in_raw_fasta_path,
+                                'out_fasta': out_rc_fasta_path}
+            cmd_cutadapt_tag_str = "cutadapt --cores={num_threads} --no-indels -e 0 --trimmed-only " \
+                               "-g '{tag_fwd};min_overlap={tag_fwd_len}...{tag_rev_rc};min_overlap={tag_rev_rc_len}' " \
+                               "-o {out_fasta} {in_fasta_path}".format(**cmd_cutadapt_tag_dic)
+
+            Logger.instance().debug("Running: {}".format(cmd_cutadapt_tag_str))
+            run_result = subprocess.run(shlex.split(cmd_cutadapt_tag_str), stdout=subprocess.PIPE)
+            Logger.instance().info(str(run_result.stdout))
+
+            ############################################################################################################
+            #
+            # Trim primers from output
+            # (Error allowed, 0.1 by default; minimum overlap: length of the primer; no indel; multithreading)
+            #
+            ############################################################################################################
+            #   cutadapt --cores=THREADS --no-indels -e ERROR_RATE -m MINIMUM_LENGTH -M MAXIMUM_LENGTH --trimmed-only
+            #   -g "PRIMER_FW_SEQ;min_overlap=LENGTH_OF_PRIMER_FW...PRIMER_REV_REVCOMP_SEQ;min_overlap=LENGTH_OF_PRIMER_REV"
+            #   -o TRIMMED_MARKER_RUN_SAMPLE_REPLICATE.fasta DEMULT_MARKER_RUN_SAMPLE_REPLICATE.fasta
+
+            primer_fwd_rc = str(Seq(primer_fwd, generic_dna).reverse_complement())
+            in_fasta_path = out_rc_fasta_path
+            out_rc_fasta_basename = os.path.basename(in_fasta_path).replace('_rc_sorted_%03d.fasta'%i, '_rc_sorted_trimmed_%03d.fasta'%i)
+            out_rc_fasta_path = os.path.join(tempdir, out_rc_fasta_basename)
+
+            cmd_cutadapt_primer_dic = {'primer_fwd': primer_rev, 'primer_fwd_len': len(primer_rev), 'primer_rev_rc': primer_fwd_rc,
+                        'primer_rev_rc_len': len(primer_fwd_rc), 'num_threads': num_threads, 'in_fasta_path': in_fasta_path,
+                                'out_fasta': out_rc_fasta_path, 'error_rate': 0.1, 'read_min_length': read_min_length,
+                                       'read_max_length': read_max_length}
+            cmd_cutadapt_primer_str = "cutadapt --cores={num_threads} --no-indels -e {error_rate} -m {read_min_length} " \
+                                      "-M {read_max_length} --trimmed-only  " \
+                                      "-g '{primer_fwd};min_overlap={primer_fwd_len}...{primer_rev_rc};min_overlap={primer_rev_rc_len}' " \
+                               "-o {out_fasta} {in_fasta_path}".format(**cmd_cutadapt_primer_dic)
+
+            Logger.instance().debug("Running: {}".format(cmd_cutadapt_primer_str))
+            run_result = subprocess.run(shlex.split(cmd_cutadapt_primer_str), stdout=subprocess.PIPE)
+            Logger.instance().info(str(run_result.stdout))
+
+            ############################################################################################################
+            #
+            # Reverse complement back rc fasta and pool
+            #
+            ############################################################################################################
+
+            out_final_fasta_basename = os.path.basename(in_raw_fasta_path)
+            out_final_fasta_path = os.path.join(outdir, out_final_fasta_basename)
+            shutil.copy(out_fasta_path, out_final_fasta_path)
+
+            Logger.instance().debug("Pooling fwd and rc reads...")
+            with open(out_final_fasta_path, 'a') as fout:
+                with open(out_rc_fasta_path, 'r') as fin:
+                    for line in fin:
+                        if not line.startswith('>'):
+                            fout.write("%s\n"%str(Seq(line.strip(), generic_dna).reverse_complement()))
+                        else:
+                            fout.write(line)
+
+            fasta_info_df_i.drop(['tagfwd', 'primerfwd', 'tagrev', 'primerrev', 'fastqfwd', 'fastqrev'], axis=1, inplace=True)
+            fasta_info_df_i['sorted'] = out_final_fasta_basename
 
         fasta_trimmed_info_tsv = os.path.join(outdir, 'readinfo.tsv')
         sorted_read_info_df.to_csv(fasta_trimmed_info_tsv, sep="\t", header=True, index=False)
