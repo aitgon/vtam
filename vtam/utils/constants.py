1033901089a39540b528a0eb1d7cc57a1e24792c
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-04-10 17:02:05 +0200

    ENH uchime3_denovo
    Former-commit-id: 13f597d326d15857f30401118b8936de881d3e64

--- vtam/utils/constants.py
+++ vtam/utils/constants.py
@@ -21,6 +21,8 @@ parameters_numerical_default = {
     'cutadapt_maximum_length': 500,
     # Filter/VariantReadCount parameters
     'global_read_count_threshold': 2,
+    # vsearch/Chimera detection/uchime3_denovo
+    'uchime3_denovo_abskew': 16.0,
     #
     'genetic_table_number': 5,
     'ltg_rule_threshold': 97,
