434afc6843c659128a1fa83e9a249ae5b8ee2efb
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-05-10 13:34:42 +0200

    RFR
    Former-commit-id: 582e95cca81bd8f2f3f22c916bd6c87cf9c40360

--- vtam/wrapper/SampleInformation.py
+++ vtam/wrapper/SampleInformation.py
@@ -1,6 +1,8 @@
 from wopmars.models.ToolWrapper import ToolWrapper
 import pandas
 
+from vtam.utils.SampleInformationFile import SampleInformationFile
+
 
 class SampleInformation(ToolWrapper):
     __mapper_args__ = {
@@ -35,59 +37,17 @@ class SampleInformation(ToolWrapper):
 
     def run(self):
         session = self.session
-        engine = session._session().get_bind()
 
-        ##########################################################
+        ################################################################################################################
         #
         # Wrapper inputs, outputs and parameters
         #
-        ##########################################################
+        ################################################################################################################
 
         # input file paths
         csv_path = self.input_file(SampleInformation.__input_file_csv)
-        #
-        # Models
-        biosample_model = self.output_table(SampleInformation.__output_table_biosample)
-        fasta_model = self.output_table(SampleInformation.__output_table_sorted_read_file)
-        marker_model = self.output_table(SampleInformation.__output_table_marker)
-        run_model = self.output_table(SampleInformation.__output_table_run)
-        sampleinformation_model = self.output_table(SampleInformation.__output_table_sample_information)
 
-        sorted_read_info_df = pandas.read_csv(csv_path, sep="\t", header=0)
-        sorted_read_info_df.columns = sorted_read_info_df.columns.str.lower()
-        for row in sorted_read_info_df.itertuples():
-
-            run_name = row.run
-            marker_name = row.marker
-            biosample_name = row.biosample
-            replicate = row.replicate
-            sorted_read_file = row.sortedfasta
-            #
-            # Insert run
-            run_obj = {'name': run_name}
-            run_instance = SampleInformation.get_or_create(session, run_model, **run_obj)
-            run_id = run_instance.id
-            #
-            # Insert marker_id
-            marker_obj = {'name': marker_name}
-            marker_instance = SampleInformation.get_or_create(session, marker_model, **marker_obj)
-            marker_id = marker_instance.id
-            #
-            # Insert Biosamples
-            biosample_obj = {'name': biosample_name}
-            biosample_instance = SampleInformation.get_or_create(session, biosample_model, **biosample_obj)
-            biosample_id = biosample_instance.id
-            #
-            # Insert file output
-            fasta_obj = {'name': sorted_read_file, 'run_id': run_id}
-            fasta_instance = SampleInformation.get_or_create(session, fasta_model, **fasta_obj)
-            fasta_id = fasta_instance.id
-
-            # Insert sample_information
-            sample_information_obj = {'biosample_id': biosample_id, 'replicate': replicate, 'run_id': run_id}
-            sample_information_obj['sortedreadfile_id'] = fasta_id
-            sample_information_obj['marker_id'] = marker_id
-            SampleInformation.get_or_create(session, sampleinformation_model, **sample_information_obj)
+        SampleInformationFile(csv_path).to_sqlite(session=session)
 
         ################################################################################################################
         #
