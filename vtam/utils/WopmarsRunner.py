18a203745fdd1f251623871700d24c46f02a5c1c
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-06-03 07:24:25 +0200

    ENH add cutoff specific function
    Former-commit-id: 1a5b51177e7287ef75076c9c8caaf67b27e7731d

--- vtam/utils/WopmarsRunner.py
+++ vtam/utils/WopmarsRunner.py
@@ -80,13 +80,19 @@ class WopmarsRunner(Singleton):
             # Create wopfile
             wopfile_path = os.path.join(self.tempdir, 'wopfile_filter.yml')
 
-            if not self.cli_args_and_numerical_params['lfn_variant_replicate']:  # lfn_variant algorithm
+            if self.cli_args_and_numerical_params['lfn_variant_replicate']:  # lfn_variant replicate algorithm
+                self.cli_args_and_numerical_params['lfn_variant_cutoff'] = None
+                self.cli_args_and_numerical_params['lfn_variant_specific_cutoff'] = None
+                if not (self.cli_args_and_numerical_params['cutoff_specific'] is None):
+                    self.cli_args_and_numerical_params['lfn_variant_replicate_specific_cutoff'] = self.cli_args_and_numerical_params['cutoff_specific']
 
-                self.cli_args_and_numerical_params['lfn_variant_specific_cutoff'] = self.cli_args_and_numerical_params['cutoff_specific']
-
-            else:  # lfn_variant_replicate algorithm
-
-                self.cli_args_and_numerical_params['lfn_variant_replicate_specific_cutoff'] = self.cli_args_and_numerical_params['cutoff_specific']
+            else:  # lfn_variant algorithm
+                self.cli_args_and_numerical_params['lfn_variant_replicate_cutoff'] = None
+                self.cli_args_and_numerical_params['lfn_variant_replicate_specific_cutoff'] = None
+                if self.cli_args_and_numerical_params['cutoff_specific'] is None:
+                    self.cli_args_and_numerical_params['lfn_variant_specific_cutoff'] = None
+                else:
+                    self.cli_args_and_numerical_params['lfn_variant_specific_cutoff'] = self.cli_args_and_numerical_params['cutoff_specific']
 
         ############################################################################################
         #
