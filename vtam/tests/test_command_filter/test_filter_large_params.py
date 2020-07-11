6668cfeebfa75de2700b8bf6de3e9b1cb30cc0d4
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-07-11 16:45:55 +0200

    TST Fixed tests in windows
    Former-commit-id: f4867921d3596d175af6b00135614f079840e42c

--- vtam/tests/test_command_filter/test_filter_large_params.py
+++ vtam/tests/test_command_filter/test_filter_large_params.py
@@ -1,3 +1,6 @@
+from urllib import request
+from vtam.utils.PathManager import PathManager
+from vtam.utils.constants import sorted_dryad_f40v5_tar_gz_url
 import filecmp
 import os
 import pathlib
@@ -9,37 +12,28 @@ import tarfile
 import unittest
 import urllib
 
-from vtam.utils.PathManager import PathManager
-from vtam.utils.constants import sorted_dryad_f40v5_tar_gz_url
-from urllib import request
 
 @unittest.skipIf(request.urlopen(sorted_dryad_f40v5_tar_gz_url).getcode() != 200,
                  "This test requires an internet connection!")
 class TestFilterLargeParams(unittest.TestCase):
 
-    """Will test main commands based on a complete test dataset"""
-
     @classmethod
     def setUpClass(cls):
 
+        cls.package_path = os.path.join(PathManager.get_package_path())
         # vtam needs to be in the tsv_path
-        cmd = '{} -m pip install . --upgrade'.format(sys.executable)
-        if sys.platform.startswith("win"):
-            args = cmd
-        else:
-            args = shlex.split(cmd)
-        subprocess.run(args=args, check=True, cwd=PathManager.get_package_path())
+        subprocess.run([sys.executable, '-m', 'pip', 'install', '.', '--upgrade'], cwd=cls.package_path)
 
         cls.package_path = os.path.join(PathManager.get_package_path())
         cls.test_path = os.path.join(PathManager.get_test_path())
         cls.outdir_path = os.path.join(cls.test_path, 'outdir')
+        cls.outdir_data_path = os.path.join(cls.outdir_path, 'data')
         shutil.rmtree(cls.outdir_path, ignore_errors=True)
-        pathlib.Path(cls.outdir_path).mkdir(parents=True, exist_ok=True)
-        os.environ['VTAM_LOG_VERBOSITY'] = str(10)
+        pathlib.Path(cls.outdir_data_path).mkdir(parents=True, exist_ok=True)
 
         ############################################################################################
         #
-        # Download fastq test dataset
+        # Download sorted reads dataset
         #
         ############################################################################################
 
@@ -47,11 +41,20 @@ class TestFilterLargeParams(unittest.TestCase):
         if not os.path.isfile(sorted_tar_path):
             urllib.request.urlretrieve(sorted_dryad_f40v5_tar_gz_url, sorted_tar_path)
         tar = tarfile.open(sorted_tar_path, "r:gz")
-        tar.extractall(path=cls.outdir_path)
+        tar.extractall(path=cls.outdir_data_path)
         tar.close()
+
+        ############################################################################################
+        #
+        # Args
+        #
+        ############################################################################################
 
         cls.args = {}
         cls.args['params'] = os.path.join(os.path.dirname(__file__), "params_large.yml")
+        cls.args['asvtable_default'] = os.path.join(cls.outdir_data_path, "asvtable_default.tsv")
+        cls.args['blastdbdir'] = os.path.join(cls.outdir_data_path, "coi_blast_db_dir")
+        cls.args['taxonomy'] = os.path.join(cls.outdir_data_path, "taxonomy.tsv")
 
         ############################################################################################
         #
@@ -59,13 +62,13 @@ class TestFilterLargeParams(unittest.TestCase):
         #
         ############################################################################################
 
-        cmd = "vtam taxonomy --output taxonomy.tsv --precomputed"
-
+        cmd = "vtam taxonomy --output {taxonomy} --precomputed".format(**cls.args)
+        # print(cmd)
         if sys.platform.startswith("win"):
             args = cmd
         else:
             args = shlex.split(cmd)
-        subprocess.run(args=args, check=True, cwd=cls.outdir_path)
+        subprocess.run(args=args, check=True, cwd=cls.outdir_data_path)
 
         ############################################################################################
         #
@@ -73,13 +76,13 @@ class TestFilterLargeParams(unittest.TestCase):
         #
         ############################################################################################
 
-        cmd = "vtam coi_blast_db --blastdbdir coi_blast_db_dir --blastdbname coi_blast_db_20191211"
-
+        cmd = "vtam coi_blast_db --blastdbdir {blastdbdir} --blastdbname coi_blast_db_20191211".format(**cls.args)
+        # print(cmd)
         if sys.platform.startswith("win"):
             args = cmd
         else:
             args = shlex.split(cmd)
-        subprocess.run(args=args, check=True, cwd=cls.outdir_path)
+        subprocess.run(args=args, check=True, cwd=cls.outdir_data_path)
 
         ############################################################################################
         #
@@ -88,16 +91,25 @@ class TestFilterLargeParams(unittest.TestCase):
         ############################################################################################
 
         cmd = "vtam filter --lfn_variant_replicate --db db.sqlite --readinfo sorted/readinfo.tsv " \
-              "--readdir sorted --asvtable asvtable_default.tsv --params {params}".format(**cls.args)
+              "--readdir sorted --asvtable {asvtable_default} --params {params}".format(**cls.args)
+        # print(cmd)
         if sys.platform.startswith("win"):
             args = cmd
         else:
             args = shlex.split(cmd)
-        subprocess.run(args=args, cwd=cls.outdir_path)
+        subprocess.run(args=args, cwd=cls.outdir_data_path)
+        pass
+
+    def setUp(self):
+
+        self.outdir_thistest_path = os.path.join(self.outdir_path, 'thistest')
+        # during development of the test, this prevents errors
+        pathlib.Path(self.outdir_thistest_path).mkdir(parents=True, exist_ok=True)
+        os.environ['VTAM_LOG_VERBOSITY'] = str(10)
 
     def test_01_filter(self):
 
-        asvtable_path = os.path.join(self.outdir_path, "asvtable_default.tsv")
+        asvtable_path = self.args['asvtable_default']
         asvtable_bak_path = os.path.join(self.test_path, "test_files_dryad.f40v5/asvtable_default.tsv")
         self.assertTrue(filecmp.cmp(asvtable_path, asvtable_bak_path, shallow=False))
 
@@ -106,22 +118,27 @@ class TestFilterLargeParams(unittest.TestCase):
         ############################################################################################
         #
         # Command Filter
-        #
+        # #
         ############################################################################################
 
-        cmd = "vtam taxassign --variants asvtable_default.tsv --output asvtable_default_taxa.tsv " \
-                  "--db db.sqlite --blastdbdir coi_blast_db_dir --blastdbname coi_blast_db_20191211 --taxonomy taxonomy.tsv"
-
+        cmd = "vtam taxassign --variants {asvtable_default} --output asvtable_default_taxa.tsv " \
+                  "--db db.sqlite --blastdbdir {blastdbdir} --blastdbname coi_blast_db_20191211 --taxonomy {taxonomy}".format(**self.args)
+        # print(cmd)
         if sys.platform.startswith("win"):
             args = cmd
         else:
             args = shlex.split(cmd)
-        subprocess.run(args=args, cwd=self.outdir_path)
+        subprocess.run(args=args, cwd=self.outdir_thistest_path)
 
-        asvtable_path = os.path.join(self.outdir_path, "asvtable_default_taxa.tsv")
+        asvtable_path = os.path.join(self.outdir_thistest_path, "asvtable_default_taxa.tsv")
         asvtable_bak_path = os.path.join(self.test_path, "test_files_dryad.f40v5/asvtable_default_taxa.tsv")
         self.assertTrue(filecmp.cmp(asvtable_path, asvtable_bak_path, shallow=False))
 
+    def tearDown(self):
+
+        shutil.rmtree(self.outdir_thistest_path, ignore_errors=True)
+
     @classmethod
     def tearDownClass(cls):
+
         shutil.rmtree(cls.outdir_path, ignore_errors=True)
