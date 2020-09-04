fa360f5bbde3f2e4450378df5522a70cc8696c07
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-09-04 09:03:29 +0200

    ENH Added function in test
    Former-commit-id: 61fd32606b09bfbac89db3c500b0a07185c02e0e

--- vtam/tests/test_cluster_sequences.py
+++ vtam/tests/test_cluster_sequences.py
@@ -45,8 +45,21 @@ class TestArgParser(unittest.TestCase):
         otutabout_df.rename({'#OTU ID': 'centroid'}, axis=1, inplace=True)
 
         otutabout_long_df = pandas.melt(otutabout_df, id_vars=['centroid'], var_name='variant', value_name='read_count')
+        otutabout_long_df.rename({'centroid': 'clusterid'}, axis=1, inplace=True)
         otutabout_long_df = otutabout_long_df.loc[otutabout_long_df.read_count > 0]
-        import pdb; pdb.set_trace()
+        otutabout_long_df.variant = otutabout_long_df.variant.astype('int')
+
+        cluster_count_df = otutabout_long_df[['clusterid', 'variant']].groupby('clusterid').count()
+        cluster_count_df.rename({'variant': 'clustersize'}, axis=1, inplace=True)
+        cluster_count_df = otutabout_long_df[['clusterid', 'variant']].merge(cluster_count_df, on='clusterid')
+
+        asvtable_out_df = asvtable_df.merge(cluster_count_df, on='variant')
+
+        cols = list(asvtable_out_df.columns.values)  # Make a list of all of the columns in the df
+        cols.pop(cols.index('chimera_borderline'))  # Remove b from list
+        cols.pop(cols.index('sequence'))  # Remove x from list
+        asvtable_out_df = asvtable_out_df[cols + ['chimera_borderline', 'sequence']]  # Create new dataframe with columns in the order you want
+        asvtable_out_df.to_csv('asvtable.tsv', index=False)
 
 
 
