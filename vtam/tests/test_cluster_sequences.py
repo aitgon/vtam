319fef3c8d9586a921a8e19470cb68d5a22dd905
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-09-04 15:47:39 +0200

    ENH refactor to class
    Former-commit-id: 9fdde51b41977e282ceb27ad1c85020d06438605

--- vtam/tests/test_cluster_sequences.py
+++ vtam/tests/test_cluster_sequences.py
@@ -1,15 +1,12 @@
 import os
-import shlex
-import subprocess
-import sys
-
 import pandas
 import unittest
 
 from vtam.utils.PathManager import PathManager
+from vtam.utils.SequenceClusterer import SequenceClusterer
 
 
-class TestArgParser(unittest.TestCase):
+class TestSequenceClusterer(unittest.TestCase):
 
     def setUp(self):
 
@@ -17,49 +14,15 @@ class TestArgParser(unittest.TestCase):
         self.test_path = os.path.join(PathManager.get_test_path())
         self.outdir_path = os.path.join(self.test_path, 'outdir')
 
-    def test01(self):
+    def test_compute_clusters(self):
 
         asvtable_default_tsv = os.path.join(self.test_path, "test_files_dryad.f40v5_small/run1_mfzr_zfzr/asvtable_default.tsv")
 
-        # create fasta
-        tempdir = PathManager.instance().get_tempdir()
         asvtable_df = pandas.read_csv(asvtable_default_tsv, sep='\t')
+        asvtable_df.rename({'variant': 'variant_id'}, axis=1, inplace=True)
 
-        i_fas = os.path.join(tempdir, 'cluster_input.fas')
-        with open(i_fas, 'w') as fout:
-            for idx,row in asvtable_df.iterrows():
-                valdict = {}
-                valdict['variant'] = row.variant
-                valdict['read_count'] = row.read_count
-                valdict['sequence'] = row.sequence
-                fout.write(">{variant};size={read_count}\n{sequence}\n".format(**valdict))
-        cmd = "vsearch --cluster_size cluster_input.fas --id 0.97 --otutabout otutabout.txt --clusters test"
-        if sys.platform.startswith("win"):
-            args = cmd
-        else:
-            args = shlex.split(cmd)
-        subprocess.run(args=args, check=True, cwd=tempdir)
+        seq_clusterer_obj = SequenceClusterer(asvtable_df)
+        cluster_count_df = seq_clusterer_obj.compute_clusters()
 
-        otutabout_path = os.path.join(tempdir, "otutabout.txt")
-        otutabout_df = pandas.read_csv(otutabout_path, sep="\t")
-        otutabout_df.rename({'#OTU ID': 'centroid'}, axis=1, inplace=True)
-
-        otutabout_long_df = pandas.melt(otutabout_df, id_vars=['centroid'], var_name='variant', value_name='read_count')
-        otutabout_long_df.rename({'centroid': 'clusterid'}, axis=1, inplace=True)
-        otutabout_long_df = otutabout_long_df.loc[otutabout_long_df.read_count > 0]
-        otutabout_long_df.variant = otutabout_long_df.variant.astype('int')
-
-        cluster_count_df = otutabout_long_df[['clusterid', 'variant']].groupby('clusterid').count()
-        cluster_count_df.rename({'variant': 'clustersize'}, axis=1, inplace=True)
-        cluster_count_df = otutabout_long_df[['clusterid', 'variant']].merge(cluster_count_df, on='clusterid')
-
-        asvtable_out_df = asvtable_df.merge(cluster_count_df, on='variant')
-
-        cols = list(asvtable_out_df.columns.values)  # Make a list of all of the columns in the df
-        cols.pop(cols.index('chimera_borderline'))  # Remove b from list
-        cols.pop(cols.index('sequence'))  # Remove x from list
-        asvtable_out_df = asvtable_out_df[cols + ['chimera_borderline', 'sequence']]  # Create new dataframe with columns in the order you want
-        asvtable_out_df.to_csv('asvtable.tsv', index=False)
-
-
-
+        self.assertEqual(cluster_count_df.clusterid.tolist(), [121, 1234, 1234, 1234, 1234, 1241, 1309, 1309, 1328, 144, 144, 1371, 4347, 4347, 1651, 1651, 2031, 2031, 1959, 2124, 2138, 2138, 2371, 2371, 2371, 2371, 2524, 2542, 2684, 2684, 3679, 3679, 2790, 2801, 2801, 2816, 3101, 3101, 3101, 32, 3299, 3307, 4420, 4420, 380, 380, 400, 5031, 5031, 4073, 4134, 422, 4265, 4265, 4355, 4378, 4445, 456, 456, 456, 474, 493, 507, 510, 521, 538, 553, 58, 603, 954])
+        self.assertEqual(cluster_count_df.clustersize.tolist(), [1, 4, 4, 4, 4, 1, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 4, 4, 4, 4, 1, 1, 2, 2, 2, 2, 1, 2, 2, 1, 3, 3, 3, 1, 1, 1, 2, 2, 2, 2, 1, 2, 2, 1, 1, 1, 2, 2, 1, 1, 1, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
