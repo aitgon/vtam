6c2e66fca215ae43aef7d2bcca043ac1f388b0ca
gonzalez <aitor.gonzalez@univ-amu.fr>
2020-10-13 14:22:34 +0200

    BUG fixed error due to many requests

--- vtam/CommandExample.py
+++ vtam/CommandExample.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import shlex
 import subprocess
 import sys
@@ -7,7 +8,7 @@ import urllib
 
 from vtam.utils.PathManager import PathManager
 from urllib import request
-from vtam.utils.constants import fastq_tar_gz_url2, fastq_tar_gz_url1
+from vtam.utils.constants import fastq_tar_gz_url1, fastq_tar_gz_url2, fastq_tar_gz_url3
 from vtam.utils.MyProgressBar import MyProgressBar
 
 
@@ -19,48 +20,43 @@ class CommandExample(object):
 
         package_path = PathManager.get_package_path()
 
-        ############################################################################################
+        #######################################################################
         #
         # Download fastq
         #
-        ############################################################################################
+        #######################################################################
 
-        # fastq_tar_path = os.path.join(outdir, "fastq.tar.gz")
-        # if not os.path.isfile(fastq_tar_path):
-        #     Logger.instance().info(fastq_tar_path)
-        #     urllib.request.urlretrieve(fastq_tar_gz_url1, fastq_tar_path, MyProgressBar())
-        # tar = tarfile.open(fastq_tar_path, "r:gz")
-        # tar.extractall(path=outdir)
-        # tar.close()
-
-        fastq_tar_path = os.path.join(package_path, "..", "data", "fastq.tar.gz")  # tests
-        if not os.path.isfile(fastq_tar_path):  # in the distribution
-            fastq_tar_path = os.path.join(outdir, "fastq.tar.gz")
+        fastq_tar_path = os.path.join(outdir, "fastq.tar.gz")
+        # Test first in local dir, otherwise in the remote URLs
+        if not os.path.isfile(fastq_tar_path) or pathlib.Path(fastq_tar_path).stat().st_size < 1000000:
             try:
                 urllib.request.urlretrieve(fastq_tar_gz_url1, fastq_tar_path, MyProgressBar())
-            except:
-                urllib.request.urlretrieve(fastq_tar_gz_url2, fastq_tar_path, MyProgressBar())
+            except Exception:
+                try:
+                    urllib.request.urlretrieve(fastq_tar_gz_url2, fastq_tar_path, MyProgressBar())
+                except Exception:
+                    urllib.request.urlretrieve(fastq_tar_gz_url3, fastq_tar_path, MyProgressBar())
         tar = tarfile.open(fastq_tar_path, "r:gz")
         tar.extractall(path=outdir)
         tar.close()
 
         os.remove(fastq_tar_path)
 
-        ############################################################################################
+        #######################################################################
         #
         # Set command args
         #
-        ############################################################################################
+        #######################################################################
 
         args = {}
         args['package_path'] = package_path
         args['snake_tuto_data'] = os.path.join(package_path, "data/snake.tuto.data.yml")
 
-        ############################################################################################
+        #######################################################################
         #
         # Copy data to directory tree
         #
-        ############################################################################################
+        #######################################################################
 
         cmd = "snakemake --cores 1 -s {snake_tuto_data} --config MARKER=mfzr " \
               "PROJECT=asper1 PACKAGE_PATH={package_path} --until all_one_marker".format(**args)
