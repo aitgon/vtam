76aea122a5e889a977549b96e2595ad9123204cb
Aitor Gonzalez <aitor.gonzalez@univ-amu.fr>
2020-12-16 06:07:34 +0100

    BLD updated setup.py

--- setup.py
+++ setup.py
@@ -33,33 +33,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as fin:
     long_description = fin.read()
 
-# CLASSIFIERS = """\
-# Development Status :: 4 - Beta
-# Environment :: Console
-# Intended Audience :: Science/Research
-# License :: OSI Approved :: MIT License
-# Programming Language :: Python
-# Programming Language :: Python :: 3
-# Programming Language :: Python :: 3.7
-# Programming Language :: Python :: 3 :: Only
-# Topic :: Scientific/Engineering :: Bio-Informatics
-# Operating System :: POSIX :: Linux
-# Operating System :: Microsoft :: Windows :: Windows 10
-# """
-
-
-# # Create list of package data files
-# def data_files_to_list(directory):
-#     paths = []
-#     for (path, directories, filenames) in os.walk(directory):
-#         for filename in filenames:
-#             paths.append(os.path.join('..', path, filename))
-#     return paths
-
-
-# data_file_list = data_files_to_list('vtam/data')
-# data_example_file_list = data_files_to_list('vtam/data/example')
-
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
     with codecs.open(os.path.join(here, rel_path), 'r') as fp:
@@ -73,21 +46,6 @@ def get_version(rel_path):
     else:
         raise RuntimeError("Unable to find version string.")
 
-# if sys.version_info < (3, 7):
-#     print("At least Python 3.7 is required.\n", file=sys.stderr)
-#     exit(1)
-
-# try:
-#     from setuptools import setup, find_packages
-# except ImportError:
-#     print("Please install setuptools before installing VTAM.",
-#           file=sys.stderr)
-#     exit(1)
-
-# here = os.path.abspath(os.path.dirname(__file__))
-# with open(os.path.join(here, 'README.rst'), encoding='utf-8') as fin:
-#     long_description = fin.read()
-
 CLASSIFIERS = """\
 Development Status :: 4 - Beta
 Environment :: Console
@@ -98,6 +56,7 @@ Programming Language :: Python :: 3
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3 :: Only
 Topic :: Scientific/Engineering :: Bio-Informatics
 Topic :: Software Development
@@ -120,9 +79,9 @@ setup(
     name='vtam',
     version=get_version("vtam/__init__.py"),
     description="VTAM - Validation and Taxonomic Assignation of Metabarcoding Data "
-                "is a metabarcoding pipeline. The analyses start from high throughput "
+                "is a metabarcoding pipeline. The analysis starts from high throughput "
                 "sequencing (HTS) data of amplicons of one or several metabarcoding "
-                "markers and produce an amplicon sequence variant (ASV) "
+                "markers and produces an amplicon sequence variant (ASV) "
                 "table of validated variants assigned to taxonomic groups.",
     author=author,
     author_email=email,
@@ -133,7 +92,7 @@ setup(
     packages=find_packages(),
     package_dir={'vtam': 'vtam'},
     package_data={'vtam': data_file_list},
-    install_requires=['biopython', 'cutadapt', 'jinja2', 'pandas', 'progressbar', 'pyyaml', 'sqlalchemy', 'snakemake', 'termcolor', 'wopmars'],
+    install_requires=['biopython', 'cutadapt', 'jinja2', 'pandas', 'progressbar', 'pyyaml', 'sqlalchemy', 'snakemake', 'termcolor', 'tqdm', 'wopmars'],
     entry_points={
         'console_scripts': ['vtam=vtam:main']
     },
