763336311ed9a6c785bf5a507d46e808f5f10a29
RaphaelHebert <raphaelhebert18@gmail.com>
2022-02-21 06:04:59 +0100

    added file_decompression to /utils

---
+++ vtam/utils/FileDecompression.py
@@ -0,0 +1,41 @@
+import gzip
+import shutil
+import os 
+
+class FileDeompression(object):
+    """ Decompress file to from .gz format """
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        if not os.path.exists(self.file_path):
+            self.file_path = False
+
+        self.decompressed = None
+
+    #TODO add checks if file exists, read file extension (may be already compressed to another format) 
+
+    def gzip_decompression(self):
+        """ take a file and decompress it to .gz format using the gzip package """
+
+        if not self.file_path:
+            return self.file_path
+
+        if self.file_path.endswith('.gz'):
+            path_to_decompressed_file = self.file_path
+        else:
+            return(self.path)
+
+        with gzip.open(self.file_path, 'rb') as f_in:
+            with open(path_to_decompressed_file, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        
+        if os.path.exists(path_to_decompressed_file):
+            self.decompressed = path_to_decompressed_file
+            return path_to_decompressed_file
+        return self.file_path
+
+    def delete_file(self):
+        if self.file_path and self.decompressed != self.file_path:
+            os.remove(self.file_path)
+        return self.file_path
+         
