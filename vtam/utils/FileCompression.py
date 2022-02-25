0159a2495dbae87197a1dcdbac71c2c647814f28
RaphaelHebert <raphaelhebert18@gmail.com>
2022-02-25 04:55:59 +0100

    added bz2_compression module to fileCompression.py

--- vtam/utils/FileCompression.py
+++ vtam/utils/FileCompression.py
@@ -1,4 +1,5 @@
 import gzip
+import bz2
 import shutil
 import os 
 
@@ -8,7 +9,6 @@ class FileCompression(object):
     def __init__(self, file_path):
         self.file_path = file_path
         if not os.path.exists(self.file_path):
-            print(f'{self.file_path} is false')
             self.file_path = False
 
         self.compressed = None
@@ -41,7 +41,27 @@ class FileCompression(object):
             self.compressed = path_to_compressed_file
             return path_to_compressed_file
         return self.file_path
+    
+    def bz_compression(self):
+        ''' take a file and compress it to .bz format using the bz2 package'''
 
+        if not self.file_path:
+            return self.file_path
+
+        if self.file_path.endswith('.bz2'):
+            path_to_compressed_file = self.file_path
+        else:
+            path_to_compressed_file = self.file_path + '.bz2'
+
+        with open(self.file_path, 'rb') as f_in:
+            with bz2.open(path_to_compressed_file, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        
+        if os.path.exists(path_to_compressed_file):
+            self.compressed = path_to_compressed_file
+            return path_to_compressed_file
+        return self.file_path
+    
     def delete_file(self):
         if self.file_path and self.compressed != self.file_path:
             os.remove(self.file_path)
